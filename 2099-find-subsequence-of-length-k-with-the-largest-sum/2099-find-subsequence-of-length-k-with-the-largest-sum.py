68ecc1f3d814e6e626ddc989b5d5314cdb8724f5
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-08 20:47:51 -0800

    Time: 128 ms (7.69%), Space: 14.6 MB (55.97%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.py
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.py
@@ -1,7 +1,38 @@
 class Solution:
     def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
-        n = sorted(list((x, i) for i, x in enumerate(nums)))
-        n = n[-k:]
-        n = sorted(list((i, x) for x, i in n))
-        n = list(x for _, x in n)
-        return n
+        def swap(i, j):
+            nums[i], nums[j] = nums[j], nums[i]
+
+        def partition(b, e):
+            r = randint(b, e)
+            swap(r, e)
+            p = nums[e]
+
+            l = b - 1
+            for i in range(b, e):
+                if -nums[i] < -p:
+                    l += 1
+                    swap(l, i)
+            swap(l + 1, e)
+            return l + 1
+
+        def qselect():
+            l, r = 0, len(nums) - 1
+            while l < r:
+                mid = partition(l, r)
+                if mid == k:
+                    break
+                elif mid < k:
+                    l = mid + 1
+                else:
+                    r = mid
+
+        numscopy = nums[:]
+        qselect()
+        ans = []
+        C = Counter(nums[:k])
+        for n in numscopy:
+            if C[n] > 0:
+                C[n] -= 1
+                ans.append(n)
+        return ans

c076a23899462ce72d6ea9515b177361a5400923
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-10 00:13:44 -0800

    Time: 68 ms (22.37%), Space: 14.7 MB (54.28%) - LeetHub

--- 281-zigzag-iterator/281-zigzag-iterator.py
+++ 281-zigzag-iterator/281-zigzag-iterator.py
@@ -1,21 +1,15 @@
 class ZigzagIterator:
-    def __init__(self, v1: List[int], v2: List[int]):
-        self.v = [deque(v1), deque(v2)]
-        self.vec_idx = deque(range(len(self.v)))
-        self.count = sum(len(x) for x in self.v)
+    def __init__(self, v1, v2):
+        self.data = [(len(v), iter(v)) for v in (v1, v2) if v]
 
-    def next(self) -> int:
-        idx = self.vec_idx.popleft()
-        while not self.v[idx]:
-            idx = self.vec_idx.popleft()
-        v = self.v[idx].popleft()
-        self.count -= 1
-        self.vec_idx.append(idx)
-        return v
+    def next(self):
+        len, iter = self.data.pop(0)
+        if len > 1:
+            self.data.append((len-1, iter))
+        return next(iter)
 
-    def hasNext(self) -> bool:
-        return self.count > 0
-
+    def hasNext(self):
+        return bool(self.data)
 # Your ZigzagIterator object will be instantiated and called as such:
 # i, v = ZigzagIterator(v1, v2), []
 # while i.hasNext(): v.append(i.next())

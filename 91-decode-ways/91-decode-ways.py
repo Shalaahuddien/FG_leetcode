5bc69d56cfd91a6d1ac3ee10ca12098b816eac17
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-13 16:55:37 -0800

    Time: 54 ms (15.23%), Space: 14.3 MB (68.88%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,15 +1,15 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        @cache
-        def dfs(i):
-            if i == len(s):
-                return 1
-            if s[i] == '0':
-                return 0
-            if i == len(s)-1:
-                return 1
-            ans = dfs(i+1)
-            if int(s[i:i+2]) < 27:
-                ans += dfs(i+2)
-            return ans
-        return dfs(0)
+        n = len(s)
+        # F = [1] + [0]*n
+        F = [1, 0, 0]
+
+        for i in range(1, n+1):
+            # XXX: must init F[i%3] = 0 since 2 cases can be neither/single/both valid!
+            F[i % 3] = 0
+            if s[i-1:i] != '0':
+                F[i % 3] = F[(i-1) % 3]
+            if i-2 >= 0 and s[i-2] != '0' and int(s[i-2:i]) <= 26:
+                F[i % 3] += F[(i-2) % 3]
+        # print(F)
+        return F[n % 3]

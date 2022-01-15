0536d43d4db502272615732f3aad8e5a39fea65c
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-14 23:56:32 -0800

    Time: 1236 ms (93.30%), Space: 59.2 MB (10.88%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.py
@@ -7,12 +7,12 @@ class Solution:
         points.sort(key = lambda x : x[1])
         
         arrows = 1
-        first_end = points[0][1]
-        for x_start, x_end in points:
+        x_end = points[0][1]
+        for s,e in points:
             # if the current balloon starts after the end of another one,
             # one needs one more arrow
-            if first_end < x_start:
+            if s > x_end:
                 arrows += 1
-                first_end = x_end
+                x_end = e
         
         return arrows

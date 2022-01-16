a90328bb58c870278c0c3c99b2beede05173c364
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-15 18:18:12 -0800

    Time: 128 ms (25.97%), Space: 20.7 MB (9.81%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
@@ -6,25 +6,20 @@
 #         self.right = right
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        def inorder_itr(r):
+        def inorder(r):
             if r:
-                yield from inorder_itr(r.left)
-                yield r.val
-                yield from inorder_itr(r.right)
+                inorder(r.left)
+                ino.append(r.val)
+                inorder(r.right)
 
-        def rev_inorder_itr(r):
-            if r:
-                yield from rev_inorder_itr(r.right)
-                yield r.val
-                yield from rev_inorder_itr(r.left)
-
-        left_itr, right_itr = inorder_itr(root), rev_inorder_itr(root)
-        vi, vj = next(left_itr), next(right_itr)
-        while vi < vj:
-            if vi + vj == k:
+        ino = []
+        inorder(root)
+        i, j = 0, len(ino) - 1
+        while i < j:
+            if ino[i] + ino[j] == k:
                 return True
-            elif vi + vj < k:
-                vi = next(left_itr)
+            if ino[i] + ino[j] < k:
+                i += 1
             else:
-                vj = next(right_itr)
+                j -= 1
         return False

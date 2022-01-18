df3898f4eb579fa33b4668c8778e52043ba6c23f
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-17 18:04:28 -0800

    Time: 194 ms (12.24%), Space: 14 MB (93.76%) - LeetHub

--- 788-rotated-digits/788-rotated-digits.py
+++ 788-rotated-digits/788-rotated-digits.py
@@ -1,14 +1,19 @@
 class Solution:
     def rotatedDigits(self, n: int) -> int:
         def valid(ns):
-            if not any(c in {'3', '4', '7'} for c in ns) and \
-                any(c in {'2', '5', '6', '9'} for c in ns):
+            # if not any(c in {'3', '4', '7'} for c in ns) and \
+            #     any(c in {'2', '5', '6', '9'} for c in ns):
+            #     return True
+            cond1 = len(ns.intersection({'2', '5', '6', '9'})) > 0
+            cond2 = len(ns.intersection({'3', '4', '7'})) == 0
+            # print(ns, cond1, cond2)
+            if cond1 & cond2:
                 return True
             # 0, 1,8
             return False
 
         ans = 0
         for i in range(1, n + 1):
-            if valid(str(i)):
+            if valid(set(str(i))):
                 ans += 1
         return ans

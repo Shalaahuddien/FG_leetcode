15ad3da860ff2c20970d21ab3d8dd3c80681364a
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-17 19:08:02 -0800

    Time: 38 ms (43.12%), Space: 14.3 MB (68.23%) - LeetHub

--- 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.py
@@ -6,24 +6,26 @@
 #         self.right = right
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if root is None:
+        if not root:
             return []
+
+        # (col, node)
+        q = deque([(0, root)])
+        row = 0
         pos = defaultdict(list)
-        cols = [0, 0]
+        mnc, mxc = 0, 0
+        while q:
+            for _ in range(len(q)):
+                col, nod = q.popleft()
+                mnc, mxc = min(col, mnc), max(col, mxc)
+                pos[col].append(nod.val)
+                if nod.left:
+                    q.append((col - 1, nod.left))
+                if nod.right:
+                    q.append((col + 1, nod.right))
 
-        def dfs(r, row, col):
-            nonlocal cols
-            if r:
-                cols = [min(col, cols[0]), max(col, cols[1])]
-                dfs(r.left, row + 1, col - 1)
-                pos[col].append((row, (r.val)))
-                dfs(r.right, row + 1, col + 1)
-
-        dfs(root, 0, 0)
-
-        res = []
-        for c in range(cols[0], cols[1] + 1):
-            res.append(
-                [v for r, v in sorted(pos[c], key=lambda tu: (tu[0]))])
-            # res.append([v for r, v in pos[c]])
-        return res
+            # row += 1
+        ans = []
+        for c in range(mnc, mxc + 1):
+            ans.append(pos[c])
+        return ans

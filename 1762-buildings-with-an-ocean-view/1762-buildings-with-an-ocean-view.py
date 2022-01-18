481d047d624133d36e9f4d0db3386ae441340fff
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-18 00:07:47 -0800

    Time: 788 ms (46.94%), Space: 31.7 MB (59.91%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -2,13 +2,12 @@ class Solution:
     def findBuildings(self, heights: List[int]) -> List[int]:
         l = len(heights)
         stk = []
-        ng = [-1] * l
         ans = []
         for i in range(l - 1, -1, -1):
             while stk and heights[stk[-1]] < heights[i]:
                 stk.pop()
-            ng[i] = stk[-1] if stk else -1
-            if ng[i] == -1:
+            if not stk:
                 ans.append(i)
             stk.append(i)
-        return ans[::-1]
+        ans.reverse()
+        return ans

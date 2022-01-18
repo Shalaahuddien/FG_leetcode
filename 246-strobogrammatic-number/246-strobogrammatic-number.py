542a04aacdf179eee2671926cb63c93317486afd
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-17 18:14:45 -0800

    Time: 38 ms (33.16%), Space: 14.4 MB (9.58%) - LeetHub

---
+++ 246-strobogrammatic-number/246-strobogrammatic-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isStrobogrammatic(self, num: str) -> bool:
+        d1 = '01869'
+        d2 = '01896'
+        mp = {}
+        for a,b in zip(d1,d2):
+            mp[a] = b
+        
+        l,r = 0, len(num)-1
+        while l <= r:
+            a,b = num[l],num[r]
+            if a not in mp:
+                return False
+            if mp[a] != b:
+                return False
+            l,r = l+1,r-1
+        return True

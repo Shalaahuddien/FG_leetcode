34a8d4b6488ec4c4df1e878f32dd8c2f21624d65
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-17 20:18:58 -0800

    Time: 60 ms (72.80%), Space: 14.9 MB (10.31%) - LeetHub

--- 311-sparse-matrix-multiplication/311-sparse-matrix-multiplication.py
+++ 311-sparse-matrix-multiplication/311-sparse-matrix-multiplication.py
@@ -1,24 +1,23 @@
 class Solution:
     def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
-        I, J = len(mat1), len(mat1[0])
-        K = len(mat2[0])
+        def constructmap(mat):
+            hashmap = {}
+            rows, cols = len(mat), len(mat[0])
+            for r in range(rows):
+                for c in range(cols):
+                    if mat[r][c] != 0:
+                        hashmap[(r, c)] = mat[r][c]
+            return hashmap
 
-        nz1 = defaultdict(set)
-        for i in range(I):
-            for j in range(J):
-                if mat1[i][j]:
-                    nz1[j].add((i))
+        map1, map2 = constructmap(mat1), constructmap(mat2)
 
-        nz2 = defaultdict(set)
-        for j in nz1:
-            for k in range(K):
-                if mat2[j][k]:
-                    nz2[j].add((k))
+        m, k, n = len(mat1), len(mat1[0]), len(mat2[0])
 
-        ans = [[0] * K for _ in range(I)]
+        res = [[0 for _ in range(n)] for _ in range(m)]
 
-        for j in nz1:
-            for i in nz1[j]:
-                for k in nz2[j]:
-                    ans[i][k] += mat1[i][j] * mat2[j][k]
-        return ans
+        for pos1, val1 in map1.items():
+            for pos2, val2 in map2.items():
+                if pos1[1] == pos2[0]:
+                    res[pos1[0]][pos2[1]] += val1 * val2
+
+        return res

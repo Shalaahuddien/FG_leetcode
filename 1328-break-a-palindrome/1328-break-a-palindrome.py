8d5157a4621a50eafc7e81c60ad6c67bcc7f1734
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-17 22:04:04 -0800

    Time: 40 ms (30.73%), Space: 14.1 MB (73.34%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -1,20 +1,7 @@
 class Solution:
-    def breakPalindrome(self, palindrome: str) -> str:
-        l = len(palindrome)
-        ans = []
-        if l == 1:
-            return ''
-        i = 0
-        while i < l - 1:
-            if palindrome[i] > 'a':
-                break
-            i += 1
-        
-        if i == l // 2 and l % 2 == 1:
-            ans = palindrome[:-1] + 'b'
-            return ''.join(ans)
-        c = 'a'
-        if palindrome[i] == 'a':
-            c = 'b'
-        ans = palindrome[:i] + c + palindrome[i + 1:]
-        return ''.join(ans)
+    def breakPalindrome(self, pal: str) -> str:
+        l = len(pal)
+        for i in range(l//2):
+            if pal[i] != 'a':
+                return pal[:i] + 'a' + pal[i+1:]
+        return pal[:-1] + 'b' if l >1 else ''

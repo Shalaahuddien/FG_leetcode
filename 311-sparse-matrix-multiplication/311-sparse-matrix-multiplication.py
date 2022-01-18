46bf11adfd70092d0548cc3ac95f79efe014bfc5
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-17 20:12:31 -0800

    Time: 97 ms (45.40%), Space: 14.4 MB (96.35%) - LeetHub

--- 311-sparse-matrix-multiplication/311-sparse-matrix-multiplication.py
+++ 311-sparse-matrix-multiplication/311-sparse-matrix-multiplication.py
@@ -2,11 +2,27 @@ class Solution:
     def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
         I, J = len(mat1), len(mat1[0])
         K = len(mat2[0])
-        ans = [[0] * K for _ in range(I)]
 
+        nzj1, nzj2 = set(), set()
+
+        nz1 = defaultdict(set)
         for i in range(I):
             for j in range(J):
                 if mat1[i][j]:
-                    for k in range(K):
-                        ans[i][k] += mat1[i][j] * mat2[j][k]
+                    nz1[j].add((i))
+                    nzj1.add(j)
+
+        nz2 = defaultdict(set)
+        for j in range(J):
+            for k in range(K):
+                if mat2[j][k]:
+                    nz2[j].add((k))
+                    nzj2.add(j)
+
+        ans = [[0] * K for _ in range(I)]
+
+        for j in nzj1.intersection(nzj2):
+            for i in nz1[j]:
+                for k in nz2[j]:
+                    ans[i][k] += mat1[i][j] * mat2[j][k]
         return ans

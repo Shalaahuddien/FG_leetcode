20aa42cbfe964c763fa5c131197c449febe0f785
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-19 17:09:17 -0800

    Time: 198 ms (75.00%), Space: 15.7 MB (65.91%) - LeetHub

--- 1908-game-of-nim/1908-game-of-nim.py
+++ 1908-game-of-nim/1908-game-of-nim.py
@@ -1,8 +1,22 @@
 class Solution:
     def nimGame(self, piles: List[int]) -> bool:
-        xor = piles[0]
-        for i in range(1, len(piles)):
-            xor ^= piles[i]
-        if xor == 0:
+        @cache
+        def dfs(piles):
+            # state has no relation to order: [1,2,3]  = [2, 3,1] = ...
+            if sum(piles) == 0:
+                # Lose if no pile to remove
+                return False
+
+            lpiles = list(piles)
+            for i in range(len(lpiles)):
+                for x in range(1, lpiles[i] + 1):
+                    lpiles[i] -= x
+                    # For each pile try to remove any number, if any can cause the opponent lose, then win.
+                    if not dfs(tuple(sorted(lpiles))):
+                        return True
+                    ## backtracking
+                    lpiles[i] += x
             return False
-        return True
+
+        # make [1,2,3] to '123'
+        return dfs(tuple(sorted(piles)))

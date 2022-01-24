149c0ad57a4894d3f25545a941c131690b74e1cf
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-24 13:48:45 -0800

    Time: 43 ms (31.17%), Space: 14.2 MB (73.23%) - LeetHub

--- 1556-thousand-separator/1556-thousand-separator.py
+++ 1556-thousand-separator/1556-thousand-separator.py
@@ -1,16 +1,5 @@
 class Solution:
     def thousandSeparator(self, n: int) -> str:
-        s = str(n)
-        if len(s) < 4:
-            return s
-
-        s = s[::-1]
-        ret = [None] * len(s)
-        for i in range(len(s)):
-            ret[i] = s[i]
-            if (i + 1) % 3 == 0:
-                ret[i] += '.'
-        ans = ''.join(ret)[::-1]
-        if ans[0] == '.':
-            return ans[1:]
-        return ans
+        s = str(n)[::-1]
+        res = '.'.join(s[i:i+3] for i in range(0, len(s), 3))
+        return res[::-1]

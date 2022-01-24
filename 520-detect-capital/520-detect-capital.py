9ce136568577e437c912168f092d2905ce634c7e
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-24 09:43:48 -0800

    Time: 52 ms (19.70%), Space: 14.3 MB (46.33%) - LeetHub

--- 520-detect-capital/520-detect-capital.py
+++ 520-detect-capital/520-detect-capital.py
@@ -1,7 +1,16 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        
-        cnt = sum(c.isupper() for c in word)
-        return cnt == len(word) \
-            or cnt == 0 \
-                or cnt == 1 and word[0].isupper()
+        n = len(word)
+        if n == 1:
+            return True
+        # case 1: all cap
+        if word[0].isupper() and word[1].isupper():
+            for i in range(2, n):
+                if not word[i].isupper():
+                    return False
+        # case 2 and 3
+        else:
+            for i in range(1, n):
+                if word[i].isupper():
+                    return False
+        return True

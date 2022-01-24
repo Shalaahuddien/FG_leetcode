d304d4f2af69d1cc826b2e32a07165e69b4fc9ee
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-24 14:15:23 -0800

    Time: 68 ms (5.01%), Space: 14.3 MB (7.45%) - LeetHub

--- 504-base-7/504-base-7.py
+++ 504-base-7/504-base-7.py
@@ -1,14 +1,9 @@
 class Solution:
     def convertToBase7(self, num: int) -> str:
-        if num == 0:
-            return '0'
-        neg = num < 0
-        x = abs(num)
-        ans = []
+        x, ans, time = abs(num), 0, 1
         while x:
             x, m = divmod(x, 7)
-            ans.append(str(m))
-        ret = ''.join(ans[::-1])
-        if neg:
-            return '-' + ret
-        return ret
+            ans += time * m
+            time *= 10
+        ret = str(ans)
+        return '-' * (num < 0) + ret

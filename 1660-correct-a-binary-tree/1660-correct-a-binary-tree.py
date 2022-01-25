8a9c9184904d7efef8aa54ad7424024acf6d5ffe
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-25 11:54:00 -0800

    Time: 188 ms (76.95%), Space: 27.9 MB (37.18%) - LeetHub

--- 1660-correct-a-binary-tree/1660-correct-a-binary-tree.py
+++ 1660-correct-a-binary-tree/1660-correct-a-binary-tree.py
@@ -6,14 +6,20 @@
 #         self.right = right
 class Solution:
     def correctBinaryTree(self, root: TreeNode) -> TreeNode:
+        q = deque([(root, None)])
         seen = set()
-
-        def dfs(r: TreeNode):
-            if not r or (r.right and r.right in seen):
-                return None
-            seen.add(r)
-            r.right = dfs(r.right)
-            r.left = dfs(r.left)
-            return r
-
-        return dfs(root)
+        while q:
+            for _ in range(len(q)):
+                r, par = q.popleft()
+                seen.add(r)
+                if r.right:
+                    if r.right in seen:
+                        if par.left == r:
+                            par.left = None
+                        else:
+                            par.right = None
+                    else:
+                        q.append((r.right, r))
+                if r.left:
+                    q.append((r.left, r))
+        return root

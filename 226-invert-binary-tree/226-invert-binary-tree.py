f99fa0225a76543914859657cd34107e5b55da2c
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-26 12:43:57 -0800

    Time: 52 ms (18.29%), Space: 14.3 MB (11.55%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.py
+++ 226-invert-binary-tree/226-invert-binary-tree.py
@@ -6,12 +6,26 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        def dfs(r):
-            if not r:
-                return None
-            left,right = r.left, r.right
-            r.left = dfs(right)
-            r.right = dfs(left)
-            return r
-        return dfs(root)
+        stk, visited = [], TreeNode(None)
+
+        def pushleftbranch(r: TreeNode):
+            while r:
+                # preorder
+                r.left, r.right = r.right, r.left
+
+                stk.append(r)
+                r = r.left
+
+        pushleftbranch(root)
+        while stk:
+            p = stk[-1]
+            # p.left subtree done, but p.right not
+            if (not p.left or p.left == visited) and p.right != visited:
+                # inorder
+                pushleftbranch(p.right)
+            # p.right subtree done
+            if not p.right or p.right == visited:
+                # postorder
+                visited = stk.pop()
                 
+        return root

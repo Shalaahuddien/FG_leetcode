ce48f933e72892e9bb4ea013f74a76704954e9c1
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-26 13:18:19 -0800

    Time: 428 ms (51.29%), Space: 18 MB (89.32%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
@@ -6,24 +6,28 @@
 #         self.right = right
 class Solution:
     def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
-        def ino(r: TreeNode, lst: list[int]):
-            if r:
-                ino(r.left, lst)
-                lst.append(r.val)
-                ino(r.right, lst)
+        stk1, stk2, res = [], [], []
+        r1, r2 = root1, root2
+        while r1 or r2 or stk1 or stk2:
+            # update both stacks
+            # by going left till possible
+            while r1:
+                stk1.append(r1)
+                r1 = r1.left
+            while r2:
+                stk2.append(r2)
+                r2 = r2.left
 
-        li1, li2 = [], []
-        ino(root1, li1)
-        ino(root2, li2)
+            # Add the smallest value into output,
+            # pop it from the stack,
+            # and then do one step right
+            if not stk2 or (stk1 and stk1[-1].val <= stk2[-1].val):
+                r1 = stk1.pop()
+                res.append(r1.val)
+                r1 = r1.right
 
-        L1, L2 = len(li1), len(li2)
-        i, j = 0, 0
-        ans = []
-        while i < L1 and j < L2:
-            if li1[i] < li2[j]:
-                ans.append(li1[i])
-                i += 1
             else:
-                ans.append(li2[j])
-                j += 1
-        return ans + li1[i:] + li2[j:]
+                r2 = stk2.pop()
+                res.append(r2.val)
+                r2 = r2.right
+        return res

cd009af8806f62ad3327e677fcb51c9999872240
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-01-31 15:26:43 -0800

    Time: 1516 ms (88.46%), Space: 21.5 MB (29.09%) - LeetHub

--- 568-maximum-vacation-days/568-maximum-vacation-days.py
+++ 568-maximum-vacation-days/568-maximum-vacation-days.py
@@ -1,14 +1,17 @@
 class Solution:
     def maxVacationDays(self, flights: List[List[int]], days: List[List[int]]) -> int:
-      N, K = len(days), len(days[0])
-      flights = [[i for i, can_fly in enumerate(city) if can_fly] 
+        N, K = len(days), len(days[0])
+        F = [[i for i, can_fly in enumerate(city) if can_fly]
              for city in flights]
-      @cache
-      def backtrack(week, city):
-          if week == K:
-              return 0
-          stay = days[city][week] + backtrack(week+1, city)
-          fly = max((days[other][week] + backtrack(week+1, other) 
-                    for other in flights[city]), default=0)
-          return max(stay, fly)
-      return backtrack(0, 0)
+
+        @cache
+        def backtrack(week, city):
+            if week == K:
+                return 0
+            stay = days[city][week] + backtrack(week + 1, city)
+            fly = max((days[other][week] + backtrack(week + 1, other)
+                       for other in F[city]),
+                      default=0)
+            return max(stay, fly)
+
+        return backtrack(0, 0)

b43c05be287f24364dcae2ad22643b26a36a8331
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-02-05 14:13:18 -0800

    Time: 57 ms (20.98%), Space: 13.8 MB (99.48%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,8 +1,5 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         C = Counter(nums)
-        f = lambda x: x*(x-1)//2
-        ans = 0
-        for k,v in C.items():
-            ans += f(v)
-        return ans
+        f = lambda x: x * (x - 1) // 2
+        return sum(f(v) for v in C.values())

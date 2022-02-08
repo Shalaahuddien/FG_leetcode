237f36f7937d8574b5b675210020eaf2664f588b
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-02-08 12:43:02 -0800

    Time: 79 ms (5.30%), Space: 13.8 MB (97.72%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.py
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.py
@@ -3,22 +3,10 @@ class Solution:
         def sum2slice(lis):
             return sum(int(''.join(map(str, li))) for li in lis)
 
-        def bt(A, used, news, res):
-            if len(used) == 4:
-                mn = min(
-                    sum2slice((news[:i], news[i:])) for i in range(1, 4))
-                res.append(mn)
-                return
-            for i in range(4):
-                if i > 0 and A[i - 1] == A[i] and i - 1 not in used:
-                    continue
-                if i not in used:
-                    used.add(i)
-                    bt(A, used, news + [A[i]], res)
-                    used.remove(i)
+        perms = permutations([int(c) for c in str(num)])
 
-        A = [int(c) for c in str(num)]
-        A.sort()
-        res = []
-        bt(A, set(), [], res)
-        return min(res)
+        mn = 9999
+        for p in perms:
+            mn = min(mn,
+                     min(sum2slice((p[:i], p[i:])) for i in range(1, 4)))
+        return mn

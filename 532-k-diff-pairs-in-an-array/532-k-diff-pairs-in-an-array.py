b1b15d30dfcfd29578551cd6ab5921e5c76ce3a7
Hacken Zhang <fxrcode@users.noreply.github.com>
2022-02-08 21:31:29 -0800

    Time: 109 ms (49.12%), Space: 15.2 MB (98.31%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.py
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.py
@@ -5,14 +5,14 @@ class Solution:
         for i, x in enumerate(nums):
             if i != 0 and nums[i - 1] == x:
                 continue
-            for y in {x - k, x + k}:
-                j = bisect_right(nums, y)
-                if j == 0:
-                    continue
-                if j - 1 <= i:
-                    continue
-                if nums[j - 1] != y:
-                    continue
-                # pairs.append((x, y))
-                cnt += 1
+            y = k + x
+            j = bisect_right(nums, y)
+            if j == 0:
+                continue
+            if j - 1 <= i:
+                continue
+            if nums[j - 1] != y:
+                continue
+            # pairs.append((x, y))
+            cnt += 1
         return cnt

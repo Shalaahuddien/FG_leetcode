e1d2cdaee1b5f55f59609019c65b4f6705d3a422
林明美 <fxrcode@users.noreply.github.com>
2022-02-11 00:36:40 -0800

    Time: 154 ms (50.86%), Space: 20 MB (9.23%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
@@ -1,30 +1,20 @@
 class Solution:
     def minimumJumps(self, forbidden: List[int], a: int, b: int, x: int) -> int:
-        bad = set(forbidden)
-        footprint = set()
-        upper = max(forbidden + [x]) + a + b
-        res = [1e6]
+        vis = set(forbidden)
+        key = -1
+        UPPER = max(forbidden + [x]) + a + b
 
-        def dfs(p, cnt, prev):
-            """
-            prev: last direction. True=backed, False=not backed
-            """
-            if not 0 <= p <= upper:
-                return
-            if p in bad:
-                return
-            if (p, prev) in footprint:
-                return
-            if p == x:
-                res[0] = min(res[0], cnt)
-                return
+        def dfs(p, cnt, back):
+            nonlocal key
+            if key < 0 and 0 <= p <= UPPER:  # # 6000是往右探索的最大值，x最大为2000
+                if p == x:  # 第一次遍历到 x时的次数即为结果，暂存结果，不再递归
+                    key = cnt
+                    return
+                if p + a not in vis:
+                    vis.add(p + a)  # 防止无限递归，比如 a = b 时，不加限制，就会出现无限递归
+                    dfs(p + a, cnt + 1, 0)
+                if p - b not in vis and back != 1:  # 若back为1说明上次就是往后跳的
+                    dfs(p - b, cnt + 1, 1)
 
-            footprint.add((p, prev))
-
-            dfs(p + a, cnt + 1, False)
-            if not prev:
-                dfs(p - b, cnt + 1, True)
-
-        dfs(0, 0, True)
-        dfs(0, 0, False)
-        return -1 if res[0] == 1e6 else res[0]
+        dfs(0, 0, 0)
+        return key

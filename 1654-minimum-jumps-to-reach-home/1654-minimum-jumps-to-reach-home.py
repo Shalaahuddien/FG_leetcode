41fd1925b1ff9f34492d844a2d087489ee77ec29
林明美 <fxrcode@users.noreply.github.com>
2022-02-11 00:37:01 -0800

    Time: 100 ms (85.19%), Space: 14.9 MB (55.36%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
@@ -1,20 +1,16 @@
 class Solution:
     def minimumJumps(self, forbidden: List[int], a: int, b: int, x: int) -> int:
+        q = deque([(0, False, 0)])
         vis = set(forbidden)
-        key = -1
-        UPPER = max(forbidden + [x]) + a + b
+        while q:
+            pos, backed, jumps = q.popleft()
+            if pos == x:
+                return jumps
 
-        def dfs(p, cnt, back):
-            nonlocal key
-            if key < 0 and 0 <= p <= UPPER:  # # 6000是往右探索的最大值，x最大为2000
-                if p == x:  # 第一次遍历到 x时的次数即为结果，暂存结果，不再递归
-                    key = cnt
-                    return
-                if p + a not in vis:
-                    vis.add(p + a)  # 防止无限递归，比如 a = b 时，不加限制，就会出现无限递归
-                    dfs(p + a, cnt + 1, 0)
-                if p - b not in vis and back != 1:  # 若back为1说明上次就是往后跳的
-                    dfs(p - b, cnt + 1, 1)
-
-        dfs(0, 0, 0)
-        return key
+            pa, pb = pos + a, pos - b
+            if pa < 6000 and pa not in vis:
+                q.append((pa, False, jumps + 1))
+                vis.add(pa)
+            if not backed and pb > 0 and pb not in vis:
+                q.append((pb, True, jumps + 1))
+        return -1

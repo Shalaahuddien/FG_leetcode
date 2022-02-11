038dbae22695fee024603eeb277172d932148afe
林明美 <fxrcode@users.noreply.github.com>
2022-02-11 00:40:40 -0800

    Time: 101 ms (83.69%), Space: 14.9 MB (51.72%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
@@ -2,13 +2,15 @@ class Solution:
     def minimumJumps(self, forbidden: List[int], a: int, b: int, x: int) -> int:
         q = deque([(0, False, 0)])
         vis = set(forbidden)
+        UPPER = 6000
+
         while q:
             pos, backed, jumps = q.popleft()
             if pos == x:
                 return jumps
 
             pa, pb = pos + a, pos - b
-            if pa < 6000 and pa not in vis:
+            if pa < UPPER and pa not in vis:
                 q.append((pa, False, jumps + 1))
                 vis.add(pa)
             if not backed and pb > 0 and pb not in vis:

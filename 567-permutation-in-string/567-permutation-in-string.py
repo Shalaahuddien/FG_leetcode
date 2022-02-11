22e2429798a559afe08fd39a1250c5b70d35d88d
林明美 <fxrcode@users.noreply.github.com>
2022-02-11 10:46:49 -0800

    Time: 88 ms (70.72%), Space: 14 MB (83.82%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,20 +1,24 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        win = Counter()
-        C1 = Counter(s1)
+        o = lambda c: ord(c) - ord('a')
+
+        win = [0] * 26
+        target = [0] * 26
+        for c in s1:
+            target[o(c)] += 1
+
         l, r = 0, 0
         if len(s1) > len(s2):
             return False
         while r < len(s2):
             c = s2[r]
             r += 1
-            win[c] += 1
-            while r - l > len(s1):  # or s2[l] not in C1 or win[s2[l]] > C1[s2[l]]:
+            win[o(c)] += 1
+            while r - l > len(s1):  #! Don't over-optimize: or s2[l] not in C1 or win[s2[l]] > C1[s2[l]]:
                 d = s2[l]
                 l += 1
-                win[d] -= 1
-                if win[d] == 0:
-                    del win[d]
-            if win == C1:
+                win[o(d)] -= 1
+            # now window size === s1, do the check
+            if win == target:
                 return True
-        return win == C1
+        return win == target

dc1d9e7796b01f383f8c8f56a4697093d30fbf18
林明美 <fxrcode@users.noreply.github.com>
2022-02-15 12:03:05 -0800

    Time: 59 ms (92.92%), Space: 17.6 MB (30.46%) - LeetHub

--- 1774-closest-dessert-cost/1774-closest-dessert-cost.py
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost.py
@@ -1,19 +1,17 @@
 class Solution:
     def closestCost(self, baseCosts: List[int], toppingCosts: List[int], target: int) -> int:
-        opt = 1e6
-        ans = 0
-        for i in range(pow(3, len(toppingCosts))):
-            topps, idx, cur = 0, i, 0
-            while idx != 0:
-                idx, q = divmod(idx, 3)
-                topps += toppingCosts[cur] * (q)
-                cur += 1
-            for b in baseCosts:
-                cost = topps + b
-                if cost == target:
-                    return cost
-                if abs(cost - target) < abs(opt - target):
-                    opt = cost
-                elif abs(cost - target) == abs(opt - target):
-                    opt = min(cost, opt)
-        return opt
+        @cache
+        def fn(i, cost):
+            """
+            Return sum of subsequence closest to target.
+            """
+            if cost >= target or i == len(tC):
+                return cost
+            return min(fn(i + 1, cost), fn(i + 1, cost + tC[i]), key=closest)
+
+        tC = toppingCosts * 2
+        closest = lambda x: (abs(x - target), x)
+        ans = 1e6
+        for b in baseCosts:
+            ans = min(ans, fn(0, b), key=closest)
+        return ans

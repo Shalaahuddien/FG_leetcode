2c6a970d6cd9ec3398cf88abdfa36a2301a5f715
林明美 <fxrcode@users.noreply.github.com>
2022-02-15 12:00:41 -0800

    Time: 1510 ms (32.92%), Space: 13.9 MB (83.38%) - LeetHub

--- 1774-closest-dessert-cost/1774-closest-dessert-cost.py
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost.py
@@ -1,23 +1,19 @@
 class Solution:
     def closestCost(self, baseCosts: List[int], toppingCosts: List[int], target: int) -> int:
-        def dfs(ti, p, cost, res):
-            nonlocal ans, diff
-            if abs(cost - target) < diff:
-                ans = cost
-                diff = abs(cost - target)
-            elif abs(cost - target) == diff:
-                ans = min(ans, cost)
-            # add all combi to res
-            res.append(cost)
-            if ti == len(toppingCosts):
-                return
-            if cost > target:
-                return
-            for pick in range(3):
-                dfs(ti + 1, pick, cost + toppingCosts[ti] * pick, res)
-
-        res = []
-        ans = diff = 1e6
-        for b in baseCosts:
-            dfs(0, 0, b, res)
-        return ans
+        opt = 1e6
+        ans = 0
+        for i in range(pow(3, len(toppingCosts))):
+            topps, idx, cur = 0, i, 0
+            while idx != 0:
+                idx, q = divmod(idx, 3)
+                topps += toppingCosts[cur] * (q)
+                cur += 1
+            for b in baseCosts:
+                cost = topps + b
+                if cost == target:
+                    return cost
+                if abs(cost - target) < abs(opt - target):
+                    opt = cost
+                elif abs(cost - target) == abs(opt - target):
+                    opt = min(cost, opt)
+        return opt

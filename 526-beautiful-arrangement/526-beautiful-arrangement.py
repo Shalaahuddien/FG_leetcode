292ae47526a3166a47c4fd086b43b360ff086a0a
林明美 <fxrcode@users.noreply.github.com>
2022-02-20 16:51:37 -0800

    Time: 114 ms (90.81%), Space: 15.2 MB (16.92%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.py
+++ 526-beautiful-arrangement/526-beautiful-arrangement.py
@@ -1,14 +1,24 @@
 class Solution:
     def countArrangement(self, n: int) -> int:
-        @cache
+        memo = defaultdict(int)
+
         def dfs(bm, pl):
+            """
+            bm is binary mask for visited numbers.
+            pl is current place we want to fill. 
+            XXX: Idea is to start from the end, and fill places in opposite direction, because for big numbers we potentially have less candidates.
+            """
+            if (bm, pl) in memo:
+                # print((bm, pl))
+                return memo[(bm, pl)]
             if pl == 0:
                 return 1
             S = 0
             for i in range(n):
-                if not bm & 1 << i and ((i + 1) % pl == 0 or \
-                     pl % (i + 1) == 0):
-                    S += dfs(bm ^ 1 << i, pl - 1)
+                if not bm & (1 << i) \
+                    and ((i + 1) % pl == 0 or pl % (i + 1) == 0):
+                    S += dfs(bm ^ (1 << i), pl - 1)
+            memo[(bm, pl)] = S
             return S
 
         return dfs(0, n)

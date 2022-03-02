c16a78059a6a388e083df19a189d9e3129a29b19
林明美 <fxrcode@users.noreply.github.com>
2022-03-02 00:23:26 -0800

    Time: 106 ms (56.78%), Space: 13.9 MB (95.59%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,14 +1,24 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        def bt(start, path, res):
-            if sum(path)>target:
-                return
-            elif sum(path) == target:
+        def bt(startIdx, remain, path, res):
+            # print(startIdx, remain, path, res)
+            if remain == 0:
                 res.append(path[:])
                 return
-            
-            for i in range(start, len(candidates)):
-                bt(i, path+[candidates[i]], res)
+            for i in range(startIdx, len(candidates)):
+                c = candidates[i]
+                if c > remain:
+                    # BUG: here i+1 then recurse, but the i in for loop will redo i+1!!! So duplicate!
+                    bt(i + 1, remain, path, res)
+                    # !XXX: if you call recursion multiple times, use RETURN!!!!!!!!!!
+                    # !check labuladong sudoku backtracking
+                    return
+                else:
+                    path.append(c)
+                    bt(i, remain - c, path, res)
+                    path.pop()
+
+        candidates.sort(reverse=True)
         res = []
-        bt(0, [], res)
+        bt(0, target, [], res)
         return res

4a267fc024874d3ab6c5557b09dcace9ca2c5284
林明美 <fxrcode@users.noreply.github.com>
2022-03-02 12:09:06 -0800

    Time: 65 ms (9.90%), Space: 13.9 MB (83.74%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -1,10 +1,4 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        difa, difb = [], []
-        for x, y in zip(s1, s2):
-            if x != y:
-                difa.append(x)
-                difb.append(y)
-            if len(difa) > 2:
-                return False
-        return len(difa) == 0 or len(difa) == 2 and difa == difb[::-1]
+        dif = [[x, y] for x, y in zip(s1, s2) if x != y]
+        return not dif or len(dif) == 2 and dif[0][::-1] == dif[1]

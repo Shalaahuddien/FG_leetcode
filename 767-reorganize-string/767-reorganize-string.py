b0ed3e5af5c7363b5b3c649f8a44a8ffcb4a2085
林明美 <fxrcode@users.noreply.github.com>
2022-03-02 21:19:38 -0800

    Time: 43 ms (65.99%), Space: 13.9 MB (88.98%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.py
+++ 767-reorganize-string/767-reorganize-string.py
@@ -2,20 +2,22 @@ class Solution:
     def reorganizeString(self, s: str) -> str:
         pq = [(-f, c) for c, f in Counter(s).items()]
         heapify(pq)
-
+        mx_cnt = pq[0][0]
+        if -mx_cnt > (len(s) + 1) // 2:
+            return ""
         res = []
         while len(pq) >= 2:
             f0, c0 = heappop(pq)
             f1, c1 = heappop(pq)
             res.append(c0)
             res.append(c1)
-            if -f0 - 1 > 0:
-                heappush(pq, (f0 + 1, c0))
-            if -f1 - 1 > 0:
-                heappush(pq, (f1 + 1, c1))
+            f0 += 1
+            f1 += 1
+            if -f0 > 0:
+                heappush(pq, (f0, c0))
+            if -f1 > 0:
+                heappush(pq, (f1, c1))
         if pq:
             f, c = heappop(pq)
-            if -f > 1:
-                return ""
-            res.extend([c] * (-f))
+            res.append(c)
         return "".join(res)

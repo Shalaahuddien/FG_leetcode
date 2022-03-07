67b4dccf6a994e8b70c19d3f017ecd4a304cf746
林明美 <fxrcode@users.noreply.github.com>
2022-03-07 00:37:25 -0800

    Time: 68 ms (74.13%), Space: 21.3 MB (5.89%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -1,21 +1,13 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        if not nums or len(nums) == 0:
-          return 0
-        r = max(nums)
-        Encode = [0] * (r+1)
+        @cache
+        def rob(start):
+            if start == len(A):
+                return 0
+            return max(A[start] + (rob(start + 2) if (start + 2) < len(A) else 0), rob(start + 1) if (start + 1) < len(A) else 0)
+
+        A = [0] * (max(nums) + 1)
         for v in nums:
-          Encode[v] += v
-        
-        def rob(vals):
-          F = [0] * len(vals)
-          for i in range(len(vals)):
-            F[i] = max(
-              (F[i-2] if i >= 2 else 0) + vals[i],
-              (F[i-1] if i >= 1 else 0)
-            )
-          return F[-1]
-      
-        return rob(Encode)
-            
-          
+            A[v] += v
+
+        return rob(0)

f550ab93338117fd9fd0314272fb1111796df2e2
林明美 <fxrcode@users.noreply.github.com>
2022-03-10 01:11:21 -0800

    Time: 524 ms (66.62%), Space: 26.1 MB (31.79%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,5 +1,3 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        pigeons = len(nums)
-        holes = len(set(nums))
-        return pigeons > holes
+        return not len(nums) == len(set(nums))

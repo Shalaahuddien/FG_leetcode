a70d2a2439fbb4f9c113158e399da91658beb464
林明美 <fxrcode@users.noreply.github.com>
2022-03-10 00:42:25 -0800

    Time: 38 ms (64.54%), Space: 13.9 MB (70.65%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -1,17 +1,3 @@
 class Solution:
     def interpret(self, CMD: str) -> str:
-        stk = []
-        i = 0
-        while i < len(CMD):
-            if CMD[i] in "G(":
-                stk.append(CMD[i])
-            elif CMD[i] == "a":
-                stk.pop()
-                stk.append("al")
-                i += 1
-            elif CMD[i] == ")":
-                if stk[-1] == "(":
-                    stk.pop()
-                    stk.append("o")
-            i += 1
-        return "".join(stk)
+        return CMD.replace("()", "o").replace("(al)", "al")

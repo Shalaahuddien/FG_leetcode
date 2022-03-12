54545afe154c0cd766bb759444f5b50d60b40b7b
林明美 <fxrcode@users.noreply.github.com>
2022-03-12 14:58:00 -0800

    Time: 496 ms (89.67%), Space: 15.1 MB (86.29%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,10 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        @cache
-        def dp(i):
-            if i == len(nums)-1:
-                return True
-            if any(dp(j) for j in range(i+1, min(i+nums[i]+1, len(nums)))[::-1]):
-                return True
-            return False
-        return dp(0)
+        goal = len(nums) - 1
+        for i in range(len(nums)-1, -1, -1):
+            if i + nums[i]>= goal:
+                goal = i
+                
+        return goal == 0
+
+        

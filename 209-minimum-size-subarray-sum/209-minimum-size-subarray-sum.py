fff606b092d82f66b2fe04a578e47fdeecb957e5
林明美 <fxrcode@users.noreply.github.com>
2022-03-13 16:03:25 -0700

    Time: 111 ms (48.00%), Space: 16.7 MB (81.53%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
@@ -1,16 +1,14 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        n = len(nums)
-        if n == 0:
-            return 0
-        ans = 1e9
-        # presum[0] means sum of first 0 elements, presum[k] means sum of first k elements
-        presum = [0 for _ in range(n + 1)]
-        for i in range(1, n + 1):
-            presum[i] = presum[i - 1] + nums[i - 1]
-        for i in range(1, n + 1):
-            to_find = target + presum[i - 1]  # to_find - presum[i] = target
-            bound = bisect_left(presum, to_find)
-            if bound != len(presum):
-                ans = min(ans, (bound - (i - 1)))
-        return ans if ans != 1e9 else 0
+        l,r,n = 0,0,len(nums)
+        ans = n+1
+        wind = 0
+        while r < n:
+            wind += nums[r]
+            r += 1
+            while wind >= target:
+                ans = min(ans, r-l)
+                wind -= nums[l]
+                l += 1
+        return ans if ans != n+1 else 0
+                

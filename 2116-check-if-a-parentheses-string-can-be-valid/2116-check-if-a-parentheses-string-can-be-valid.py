b8a26c77d5a27cf7e65c491febea632ade60edac
林明美 <fxrcode@users.noreply.github.com>
2022-03-13 14:33:54 -0700

    Time: 164 ms (98.90%), Space: 18.5 MB (10.69%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.py
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.py
@@ -1,14 +1,25 @@
 class Solution:
     def canBeValid(self, s: str, locked: str) -> bool:
-        def validate(s, locked, op):
-            bal, wild = 0,0
-            for i,p in enumerate(s):
-                if locked[i] == "1":
-                    bal += 1 if p == op else -1
+        if len(s) % 2 == 1:
+            return False
+        locked_open, unlocked = [], []
+        for i, c in enumerate(s):
+            if locked[i] == "0":
+                unlocked.append(i)
+            else:
+                if c == "(":
+                    locked_open.append(i)
                 else:
-                    wild += 1
-                if wild + bal < 0:
-                    return False
-            return bal <= wild
-        
-        return len(s) % 2 == 0 and validate(s,locked, '(') and validate(s[::-1], locked[::-1], ')')
+                    if locked_open:
+                        locked_open.pop()
+                    elif unlocked:
+                        unlocked.pop()
+                    else:
+                        return False
+        while locked_open:
+            if unlocked and unlocked[-1] > locked_open[-1]:
+                unlocked.pop()
+                locked_open.pop()
+            else:
+                return False
+        return True

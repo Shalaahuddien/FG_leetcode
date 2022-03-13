1cb8603dc056a37414ef0e86811d81b8ec5a6945
林明美 <fxrcode@users.noreply.github.com>
2022-03-12 18:14:25 -0800

    Time: 2201 ms (96.30%), Space: 32.8 MB (90.37%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.py
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.py
@@ -1,25 +1,42 @@
 class Solution:
     def containsCycle(self, grid: List[List[str]]) -> bool:
-        def dfs(v, fa):
-            if v in seen:
-                return True
-            seen.add(v)
-            x, y = v
-            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                xx, yy = x + dx, y + dy
-                # !no back edge (u->v->w), so don't choose u === w.
-                if 0 <= xx < m and 0 <= yy < n and grid[xx][yy] == grid[x][y] and (xx, yy) != fa:
-                    w = (xx, yy)
-                    if dfs(w, v):
-                        return True
-            return False
+        class UF:
+            def __init__(self, n) -> None:
+                self.pa = list(range(n))
+                self.sz = [1] * n
+                self.cnt = n
 
-        seen = set()
+            def find(self, i):
+                if self.pa[i] != i:
+                    self.pa[i] = self.find(self.pa[i])
+                return self.pa[i]
+
+            def union(self, x, y):
+                if x == y:
+                    return
+                if self.sz[x] > self.sz[y]:
+                    x, y = y, x
+                self.pa[x] = y
+                self.sz[y] += self.sz[x]
+                self.cnt -= 1
+
+            def find_and_union(self, i, j):
+                # T: not connected, but cycle after union! F: already same group
+                x, y = map(self.find, [i, j])
+                if x == y:
+                    return False
+                self.union(x, y)
+                return True
+
         m, n = len(grid), len(grid[0])
+        uf = UF(m * n)
         for i in range(m):
             for j in range(n):
-                if (i, j) in seen:
-                    continue
-                if dfs((i, j), None):
-                    return True
+                if i > 0 and grid[i][j] == grid[i - 1][j]:
+                    if not uf.find_and_union((i * n+j), ((i - 1) * n + j)):
+                        return True
+
+                if j > 0 and grid[i][j] == grid[i][j - 1]:
+                    if not uf.find_and_union((i * n + j), (i * n + j - 1)):
+                        return True
         return False

30f20dda4e7e4e316337832f9a2e5ebf0995609e
林明美 <fxrcode@users.noreply.github.com>
2022-03-14 15:24:40 -0700

    Time: 1505 ms (34.96%), Space: 28.4 MB (35.60%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.py
+++ 896-monotonic-array/896-monotonic-array.py
@@ -1,15 +1,7 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
-        sign = lambda x: bool(x > 0) - bool(x < 0)
-        sn = sign(nums[-1] - nums[0])
+        inc = dec = True
         for i in range(1, len(nums)):
-            if sn == 0:
-                if not nums[i - 1] == nums[i]:
-                    return False
-            elif sn == 1:
-                if not nums[i - 1] <= nums[i]:
-                    return False
-            else:
-                if not nums[i - 1] >= nums[i]:
-                    return False
-        return True
+            inc &= nums[i - 1] <= nums[i]
+            dec &= nums[i - 1] >= nums[i]
+        return inc or dec

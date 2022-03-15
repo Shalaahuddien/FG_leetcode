94bf0d4e258440c74ae8376514b680ee5aba6767
林明美 <fxrcode@users.noreply.github.com>
2022-03-14 23:51:44 -0700

    Time: 1888 ms (5.09%), Space: 14.3 MB (27.43%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.py
@@ -0,0 +1,16 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        f = lambda c: ord(c)-ord('A')
+        n,h,d,m = len(needle), len(haystack), ord('z')-ord('A')+1, sys.maxsize
+        if n > h: return -1
+        nd, hash_n, hash_h = d**(n-1),0,0
+        # init hash
+        for i in range(n):
+            hash_n = (d*hash_n + f(needle[i])) % m
+            hash_h = (d*hash_h + f(haystack[i])) % m
+        if hash_n == hash_h: return 0
+        for i in range(1, h-n+1):
+            hash_h = (d* (hash_h - f(haystack[i-1])*nd) + f(haystack[i+n-1])) % m
+            if hash_n == hash_h:
+                return i
+        return -1

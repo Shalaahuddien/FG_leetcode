65610f2fe27eea79a85dcb38ba01011c161f51e2
林明美 <fxrcode@users.noreply.github.com>
2022-03-14 23:19:25 -0700

    Time: 188 ms (21.17%), Space: 15.5 MB (74.51%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,20 +1,21 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         AL = defaultdict(list)
-        DEG = defaultdict(int)
-        for c, p in prerequisites:
-            AL[p].append(c)
-            DEG[c] += 1
-        m = numCourses
-        q = deque([i for i in range(m) if DEG[i] == 0])
+        #! must use Dictionary Comprehension, rather defaultdict
+        DEG = {v: 0 for v in range(numCourses)}
+        for a, b in prerequisites:
+            AL[b].append(a)
+            DEG[a] += 1
+
+        q = deque([v for v in DEG if DEG[v] == 0])
         ts = []
         while q:
             u = q.popleft()
             ts.append(u)
-            if len(ts) == m:
-                return ts
+            if len(ts) == numCourses:
+                return True
             for v in AL[u]:
                 DEG[v] -= 1
                 if DEG[v] == 0:
                     q.append(v)
-        return []
+        return False

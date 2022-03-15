bcfda2476fe046ca5c658da9b237943652c216e6
林明美 <fxrcode@users.noreply.github.com>
2022-03-15 10:21:11 -0700

    Time: 36 ms (94.71%), Space: 14.2 MB (81.16%) - LeetHub

---
+++ 274-h-index/274-h-index.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse=True)
+        h = 0
+        for i, c in enumerate(citations):
+            if c > i:
+                h += 1
+            else:
+                break
+        return h

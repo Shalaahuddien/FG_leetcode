910ed150faba6dcd2faf46e18d9bd1fc76cfc832
林明美 <fxrcode@users.noreply.github.com>
2022-03-17 12:13:05 -0700

    Time: 42 ms (56.30%), Space: 13.8 MB (70.86%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.py
+++ 856-score-of-parentheses/856-score-of-parentheses.py
@@ -1,15 +1,11 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
-        @cache
-        def F(i,j):
-            ans, bal = 0,0
-            for k in range(i,j):
-                bal += 1 if s[k] == '(' else -1
-                if bal == 0:
-                    if k-i == 1:
-                        ans += 1
-                    else:
-                        ans += 2*F(i+1, k)
-                    i = k+1
-            return ans
-        return F(0, len(s))
+        bal, ans = 0, 0
+        for i,p in enumerate(s):
+            if p =='(':
+                bal += 1
+            else:
+                bal -= 1
+                if s[i-1] == '(':
+                    ans += 1 << bal
+        return ans

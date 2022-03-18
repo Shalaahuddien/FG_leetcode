b0deea858098404d654c9497e6b6378891e7bef1
林明美 <fxrcode@users.noreply.github.com>
2022-03-18 16:07:44 -0700

    Time: 1198 ms (51.56%), Space: 59.5 MB (88.67%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -1,15 +1,18 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
+        i = 0
         n = len(nums)
+        while i < n:
+            idx = nums[i] - 1
+            # put num[i] to the correct place if nums[i] in the range [1, n]
+            if 0 <= idx < n and nums[i] != nums[idx]:
+                nums[i], nums[idx] = nums[idx], nums[i]
+            else:
+                i += 1
+        # so far, all the integers that could find their own correct place
+        # have been put to the correct place, next thing is to find out the
+        # place that occupied wrongly
         for i in range(n):
-            if nums[i] < 1 or nums[i] > n:
-                nums[i] = 0
-        for i in range(n):
-            if 1 <= nums[i] % (n+1) <= n:
-                idx = nums[i] % (n+1) - 1
-                nums[idx] += n+1
-        for i in range(n):
-            if nums[i] <= n:
-                return i+1
-        return n+1
-                
+            if nums[i] != i + 1:
+                return i + 1
+        return n + 1

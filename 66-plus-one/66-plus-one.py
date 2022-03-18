b7592fc515bdf473bc997db1fa5b910730320c86
林明美 <fxrcode@users.noreply.github.com>
2022-03-18 16:22:12 -0700

    Time: 32 ms (92.17%), Space: 13.8 MB (70.13%) - LeetHub

---
+++ 66-plus-one/66-plus-one.py
@@ -0,0 +1,10 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+        for i in range(n - 1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+        return [1] + digits

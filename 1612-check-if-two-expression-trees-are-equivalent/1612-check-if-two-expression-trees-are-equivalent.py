b11438488029886b1fadd68b8d43819876338cf9
林明美 <fxrcode@users.noreply.github.com>
2022-03-18 20:02:42 -0700

    Time: 580 ms (85.15%), Space: 16.8 MB (89.11%) - LeetHub

---
+++ 1612-check-if-two-expression-trees-are-equivalent/1612-check-if-two-expression-trees-are-equivalent.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class Node(object):
+#     def __init__(self, val=" ", left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def checkEquivalence(self, root1: 'Node', root2: 'Node') -> bool:
+        def dfs(r):
+            if not r:
+                return []
+            # extend(l,r): no return, it only in-place l = l+r
+            if r.val == "+":
+                return dfs(r.left) + dfs(r.right)
+            return [r.val]
+
+        r1 = dfs(root1)
+        r2 = dfs(root2)
+        r1.sort()
+        r2.sort()
+        return r1 == r2

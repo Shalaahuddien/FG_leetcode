e6b00799865e09ffa477ec88ae06e2cedb7850e4
林明美 <fxrcode@users.noreply.github.com>
2022-03-19 23:19:13 -0700

    Time: 2103 ms (17.81%), Space: 140 MB (24.38%) - LeetHub

--- 1057-campus-bikes/1057-campus-bikes.py
+++ 1057-campus-bikes/1057-campus-bikes.py
@@ -1,16 +1,18 @@
 class Solution:
     def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) -> List[int]:
-        distances = [[] for _ in range(2001)]
+        distances = []
         for i, (x, y) in enumerate(workers):
             for j, (u, v) in enumerate(bikes):
                 d = abs(x - u) + abs(y - v)
-                distances[d].append((i, j))
+                distances.append((d, i, j))
+
+        distances.sort()
 
         used_bikes = set()
-        assign = [-1] * len(workers)
-        for dist in distances:
-            for w, b in dist:
-                if assign[w] == -1 and b not in used_bikes:
-                    used_bikes.add(b)
-                    assign[w] = b
-        return assign
+        result = [-1] * len(workers)
+        for dist, i, j in distances:
+            # print(dist, i, j)
+            if result[i] == -1 and j not in used_bikes:
+                used_bikes.add(j)
+                result[i] = j
+        return result

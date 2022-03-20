1f22e034a50636b692fd3eec054fd254bfd15a06
林明美 <fxrcode@users.noreply.github.com>
2022-03-20 13:52:47 -0700

    Time: 62 ms (49.37%), Space: 13.9 MB (45.71%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -7,17 +7,9 @@ class Solution:
                     mat[i][j], mat[j][n - i - 1], mat[n - i - 1][n - j - 1], mat[n - j - 1][i] = mat[n - j - 1][i], mat[i][j], mat[j][n - i - 1], mat[n - i - 1][n - j - 1]
             # [(i, j), (j, n - i - 1), (n - i - 1, n - j - 1), (n - j - 1, i)]
 
-        def eq_mat():
-            for r in range(len(mat)):
-                if mat[r] != target[r]:
-                    return False
-            return True
-
         n = len(mat)
-        if n == 1:
-            return mat[0] == target[0]
-        for r in range(4):
+        for _ in range(4):
             rotate90()
-            if eq_mat():
+            if mat == target:
                 return True
         return False

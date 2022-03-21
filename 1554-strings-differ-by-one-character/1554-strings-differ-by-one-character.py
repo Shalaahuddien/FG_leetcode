5bb87aac28841492c0cf93f60b05f2c2fee1a636
林明美 <fxrcode@users.noreply.github.com>
2022-03-21 01:03:16 -0700

    Time: 1349 ms (8.01%), Space: 42.4 MB (25.50%) - LeetHub

--- 1554-strings-differ-by-one-character/1554-strings-differ-by-one-character.py
+++ 1554-strings-differ-by-one-character/1554-strings-differ-by-one-character.py
@@ -1,13 +1,12 @@
 class Solution:
     def differByOne(self, dict: List[str]) -> bool:
         m = len(dict[0])
-        rm_1_char = defaultdict(int)
+        rm_1_char = set()
         for w in dict:
             s = list(w)
             for i in range(m):
                 s_rm1 = "".join(s[:i] + ["_"] + s[i + 1 :])
-                rm_1_char[s_rm1] += 1
-                # if rm_1_char[s_rm1] > 2:
-                #     return False
-        # print(rm_1_char)
-        return any(f >= 2 for f in rm_1_char.values())
+                if s_rm1 in rm_1_char:
+                    return True
+                rm_1_char.add(s_rm1)
+        return False

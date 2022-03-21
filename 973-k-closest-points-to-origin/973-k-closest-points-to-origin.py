fa753e3ea407a3ac50e021fbc30c6839cad2d6bd
林明美 <fxrcode@users.noreply.github.com>
2022-03-21 15:46:00 -0700

    Time: 1472 ms (16.43%), Space: 20.3 MB (56.62%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,7 +1,34 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        def dist(co):
-            return co[0] ** 2 + co[1] ** 2
+        dist = lambda x: x[0]**2 + x[1]**2
+        
 
-        points.sort(key=dist)
-        return points[:k]
+        def lomuto(A, L, R):
+            pi = randint(L, R)
+            A[pi], A[R] = A[R], A[pi]
+            pd = dist(A[R])
+            l = L - 1
+            for i in range(L, R):
+                # if less(A[i], p):
+                if dist(A[i]) <= pd:
+                    l += 1
+                    A[l], A[i] = A[i], A[l]
+            A[l + 1], A[R] = A[R], A[l + 1]
+            return l + 1
+        
+        def qselect_fxr(l, r):
+            """
+            Runtime: 992 ms, faster than 10.49% of Python3 online submissions for K Closest Points to Origin.
+
+            T: O(N)
+            """
+
+            pi = lomuto(points, l, r)
+            if pi == k - 1:
+                return points[:k]
+            elif pi < k - 1:
+                return qselect_fxr(pi + 1, r)
+            else:
+                return qselect_fxr(l, pi - 1)
+
+        return qselect_fxr(0, len(points) - 1)

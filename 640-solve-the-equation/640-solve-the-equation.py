effa7ad2d7f6f1c5a6261239bbadf3318f53f7b2
林明美 <fxrcode@users.noreply.github.com>
2022-03-21 13:20:32 -0700

    Time: 58 ms (15.35%), Space: 13.9 MB (90.70%) - LeetHub

--- 640-solve-the-equation/640-solve-the-equation.py
+++ 640-solve-the-equation/640-solve-the-equation.py
@@ -1,28 +1,27 @@
 class Solution:
     def solveEquation(self, equation: str) -> str:
-        def evaluate(expr):
-            tokens = expr.replace("+", "#+").replace("-", "#-").split("#")  # For example: "x+5-3+x" is replaced to "x#+5#-3#+x" then split by "#"
-            res = [0] * 2
-            for token in tokens:
-                if not token:
-                    continue
-                if token == "x" or token == "+x":
-                    res[0] += 1
-                elif token == "-x":
-                    res[0] -= 1
-                elif "x" in token:
-                    res[0] += int(token[: token.index("x")])  # For example: "3x" or "-5x" or "+6x"
-                else:
-                    res[1] += int(token)
-            return res
+        lhs, rhs = equation.split("=")
 
-        parts = equation.split("=")
-        leftRes = evaluate(parts[0])
-        rightRes = evaluate(parts[1])
-        a = leftRes[0] - rightRes[0]
-        b = rightRes[1] - leftRes[1]
-        if a == 0 and b == 0:
-            return "Infinite solutions"
-        if a == 0:
-            return "No solution"
-        return f"x={b // a}"
+        def fn(s):
+            """Parse s into coefficients."""
+            ii = x = y = 0
+            for i in range(len(s) + 1):
+                if i == len(s) or s[i] in "+-":
+                    if ii < i:
+                        y += int(s[ii:i])
+                    ii = i
+                elif s[i] == "x":
+                    if ii == i or s[ii:i] in "+-":
+                        x += int(s[ii:i] + "1")
+                    else:
+                        x += int(s[ii:i])
+                    ii = i + 1
+            return x, y
+
+        (lx, ly), (rx, ry) = fn(lhs), fn(rhs)
+        if lx == rx:
+            if ly != ry:
+                return "No solution"
+            else:
+                return "Infinite solutions"
+        return f"x={(ry-ly)//(lx-rx)}"

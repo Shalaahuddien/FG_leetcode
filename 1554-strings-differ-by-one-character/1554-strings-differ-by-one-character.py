d2acb4e71af476c01f576d39c638a1c601a6efc1
林明美 <fxrcode@users.noreply.github.com>
2022-03-21 01:05:01 -0700

    Time: 293 ms (77.67%), Space: 17.2 MB (81.00%) - LeetHub

--- 1554-strings-differ-by-one-character/1554-strings-differ-by-one-character.py
+++ 1554-strings-differ-by-one-character/1554-strings-differ-by-one-character.py
@@ -1,13 +1,20 @@
 class Solution:
     def differByOne(self, dict: List[str]) -> bool:
-        m = len(dict[0])
-        rm_1_char = defaultdict(int)
-        for w in dict:
-            s = list(w)
-            for i in range(m):
-                s_rm1 = "".join(s[:i] + ["_"] + s[i + 1 :])
-                rm_1_char[s_rm1] += 1
-                # if rm_1_char[s_rm1] > 2:
-                #     return False
-        # print(rm_1_char)
-        return any(f >= 2 for f in rm_1_char.values())
+        n, m = len(dict), len(dict[0])
+        hashes = [0] * n
+        MOD = maxsize
+
+        for i in range(n):
+            for j in range(m):
+                hashes[i] = (26 * hashes[i] + (ord(dict[i][j]) - ord("a"))) % MOD
+
+        base = 1
+        for j in range(m - 1, -1, -1):
+            seen = set()
+            for i in range(n):
+                new_h = (hashes[i] - base * (ord(dict[i][j]) - ord("a"))) % MOD
+                if new_h in seen:
+                    return True
+                seen.add(new_h)
+            base = 26 * base % MOD
+        return False

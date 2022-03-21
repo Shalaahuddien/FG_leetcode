b1045e99290f80e969bbb19b1360049423ca5f83
林明美 <fxrcode@users.noreply.github.com>
2022-03-21 00:35:08 -0700

    Time: 36 ms (80.76%), Space: 13.8 MB (69.16%) - LeetHub

--- 1805-number-of-different-integers-in-a-string/1805-number-of-different-integers-in-a-string.py
+++ 1805-number-of-different-integers-in-a-string/1805-number-of-different-integers-in-a-string.py
@@ -1,14 +1,4 @@
 class Solution:
     def numDifferentIntegers(self, word: str) -> int:
-        nums = set()
-        l, r = 0, 0
-        while l < len(word):
-            if word[l].isalpha():
-                l += 1
-            else:
-                r = l
-                while r < len(word) and word[r].isdigit():
-                    r += 1
-                nums.add(int(word[l:r]))
-                l = r
-        return len(nums)
+        s = "".join(c if c.isdigit() else " " for c in word)
+        return len(set(map(int, s.split())))

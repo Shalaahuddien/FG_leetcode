29a311457960ef7fcb39e36c365f52839cff4545
林明美 <fxrcode@users.noreply.github.com>
2022-03-21 16:08:05 -0700

    Time: 1259 ms (32.70%), Space: 20.3 MB (75.12%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -3,32 +3,35 @@ class Solution:
         dist = lambda x: x[0]**2 + x[1]**2
         
 
-        def lomuto(A, L, R):
-            pi = randint(L, R)
-            A[pi], A[R] = A[R], A[pi]
-            pd = dist(A[R])
-            l = L - 1
-            for i in range(L, R):
-                # if less(A[i], p):
-                if dist(A[i]) <= pd:
-                    l += 1
-                    A[l], A[i] = A[i], A[l]
-            A[l + 1], A[R] = A[R], A[l + 1]
-            return l + 1
+        def lomuto(A, l, r):
+            d = randint(l, r)  # random int in [l...r]
+            A[d], A[r] = A[r], A[d]
+            pdist = dist(A[r])
+            i = l - 1
+            for j in range(l, r):
+                """
+                # if A[j] < pivot:
+                invariant
+                +------+---+---+
+                |   <  | > |  p|
+                +------+---+---+
+                |     ^|   |^  |
+                |     i|   |j  |
+                +------+---+---+
+                """
+                if dist(A[j]) <= pdist:
+                    i += 1
+                    A[i], A[j] = A[j], A[i]
+            A[i + 1], A[r] = A[r], A[i + 1]
+            return i + 1
         
-        def qselect_fxr(l, r):
-            """
-            Runtime: 992 ms, faster than 10.49% of Python3 online submissions for K Closest Points to Origin.
-
-            T: O(N)
-            """
-
-            pi = lomuto(points, l, r)
-            if pi == k - 1:
-                return points[:k]
-            elif pi < k - 1:
-                return qselect_fxr(pi + 1, r)
+        l, r, p = 0, len(points) - 1, 0
+        while l < r:
+            p = lomuto(points, l, r)
+            if p == k - 1:
+                break
+            elif p > k - 1:
+                r = p - 1
             else:
-                return qselect_fxr(l, pi - 1)
-
-        return qselect_fxr(0, len(points) - 1)
+                l = p + 1
+        return points[:k]

bf3c52c6ae6de4c10764b5b7ca6dcf3ee749199a
林明美 <fxrcode@users.noreply.github.com>
2022-03-20 22:58:39 -0700

    Time: 57 ms (16.12%), Space: 13.9 MB (85.54%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.py
+++ 58-length-of-last-word/58-length-of-last-word.py
@@ -1,3 +1,10 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        return len(s.split()[-1])
+        last_c, sec_last_c = -1, -1
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] != " " and last_c == -1:
+                last_c = i
+            if last_c != -1 and s[i] == " ":
+                sec_last_c = i
+                return last_c - sec_last_c
+        return last_c + 1

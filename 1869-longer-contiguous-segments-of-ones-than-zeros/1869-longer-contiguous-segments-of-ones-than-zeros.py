928907ca053fdd08172f333901dcdecd7e591126
林明美 <fxrcode@users.noreply.github.com>
2022-03-23 00:22:30 -0700

    Time: 62 ms (11.06%), Space: 13.9 MB (35.31%) - LeetHub

--- 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.py
+++ 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.py
@@ -1,24 +1,16 @@
 class Solution:
     def checkZeroOnes(self, s: str) -> bool:
-        ones, zeros = 0, 0
-        l, r = 0, 0
-
-        o, z = False, False
-        while l < len(s):
-            if s[l] == "1":
-                o = True
-                z = False
-            else:
-                z = True
-                o = False
-            cnt = 0
-            while r < len(s) and s[r] == s[l]:
+        mx0, mx1 = 0, 0
+        cnt = 0
+        prev = "#"
+        for c in s + "#":
+            if prev == c:
                 cnt += 1
-                r += 1
-            # now r out of range or switch 0 <-> 1
-            if o:
-                ones = max(ones, r - l)
-            if z:
-                zeros = max(zeros, r - l)
-            l = r
-        return ones > zeros
+            else:
+                if prev == "0":
+                    mx0 = max(mx0, cnt)
+                elif prev == "1":
+                    mx1 = max(mx1, cnt)
+                cnt = 1
+            prev = c
+        return mx1 > mx0

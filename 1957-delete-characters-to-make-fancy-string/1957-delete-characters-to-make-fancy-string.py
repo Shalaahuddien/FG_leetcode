af3ddcac9056bf924efa91eb40d780f6ccfa4f63
林明美 <fxrcode@users.noreply.github.com>
2022-03-23 14:55:23 -0700

    Time: 769 ms (59.22%), Space: 15.9 MB (69.63%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
@@ -1,17 +1,11 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        l = r = 0
-        cnt = 0
-        ls = list(s)
-
-        while l < len(ls):
-            while r < len(ls) and ls[r] == ls[l]:
-                if cnt >= 2:
-                    ls[r] = ""
-                r += 1
+        cnt, a = 0, []
+        for i, c in enumerate(s):
+            if i > 0 and c == s[i - 1]:
                 cnt += 1
-            # r oor or ls[r] != ls[l], found segment [l:r]
-            cnt = 0
-            l = r
-
-        return "".join(ls)
+            else:
+                cnt = 1
+            if cnt < 3:
+                a.append(c)
+        return "".join(a)

28b1e4fe25d442f6184fb8850e651a674fc0e7ed
林明美 <fxrcode@users.noreply.github.com>
2022-03-23 16:02:53 -0700

    Time: 42 ms (77.50%), Space: 14 MB (65.56%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.py
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.py
@@ -1,21 +1,5 @@
 class Solution:
     def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
-        tmp = []
-        pre = 0
-        mp = {idx: i for i, idx in enumerate(indices)}
-        sortedindices = sorted(indices)
-        for i in sortedindices:
-            if not i >= pre:
-                continue
-            ori = mp[i]
-            src = sources[ori]
-            l = len(src)
-            tar = targets[ori]
-            tmp.append(s[pre:i])
-            if s[i : i + l] == src:
-                tmp.append(tar)
-            else:
-                tmp.append(s[i : i + l])
-            pre = i + l
-        tmp.append(s[pre:])
-        return "".join(tmp)
+        for i, src, tar in sorted(zip(indices, sources, targets), reverse=True):
+            s = s[:i] + tar + s[i + len(src) :] if s[i : i + len(src)] == src else s
+        return s

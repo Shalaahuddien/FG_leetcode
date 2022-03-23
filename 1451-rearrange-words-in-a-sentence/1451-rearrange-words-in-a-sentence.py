b4a5d92889fdd443d295e895eb2ace720c82642a
林明美 <fxrcode@users.noreply.github.com>
2022-03-23 15:24:09 -0700

    Time: 61 ms (59.32%), Space: 16.5 MB (39.37%) - LeetHub

--- 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.py
+++ 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.py
@@ -1,7 +1,3 @@
 class Solution:
     def arrangeWords(self, text: str) -> str:
-        words = text.split()
-        lwi = [(w, i) for i, w in enumerate(words)]
-        lwi.sort(key=lambda x: (len(x[0]), x[1]))
-        res = [w.lower() for w, i in lwi]
-        return " ".join(res).capitalize()
+        return " ".join(sorted(text.split(), key=len)).capitalize()

c6a24581330a530ac923a6b3647e83c421ebd555
林明美 <fxrcode@users.noreply.github.com>
2022-03-24 14:50:56 -0700

    Time: 1100 ms (11.93%), Space: 21.7 MB (32.60%) - LeetHub

---
+++ 854-k-similar-strings/854-k-similar-strings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def kSimilarity(self, s1: str, s2: str) -> int:
+        def nei(x):
+            i = 0
+            while x[i] == s2[i]:
+                i += 1
+            for j in range(i + 1, len(x)):
+                if x[j] == s2[i]:
+                    yield x[:i] + x[j] + x[i + 1 : j] + x[i] + x[j + 1 :]
+
+        q, seen = deque([(s1, 0)]), {s1}
+        while q:
+            x, d = q.popleft()
+            if x == s2:
+                return d
+            for y in nei(x):
+                if y not in seen:
+                    seen.add(y)
+                    q.append((y, d + 1))

57aefbdf72876599ce30cd801d747cea89591c83
林明美 <fxrcode@users.noreply.github.com>
2022-03-24 12:06:40 -0700

    Time: 452 ms (95.66%), Space: 20.9 MB (80.59%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.py
+++ 881-boats-to-save-people/881-boats-to-save-people.py
@@ -1,15 +1,11 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
         people.sort()
-        q = deque(people)
-        boats = 0
-        while q:
-            if len(q) == 1:
-                return boats+1
-            if q[0] + q[-1] > limit:
-                q.pop()
-            else:
-                q.popleft()
-                q.pop()
-            boats += 1
-        return boats
+        i, j = 0, len(people) - 1
+        ans = 0
+        while i <= j:
+            ans += 1
+            if people[i] + people[j] <= limit:
+                i += 1
+            j -= 1
+        return ans

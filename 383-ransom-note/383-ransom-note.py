78c5f10843561d1063dd4afe0557d3c9da62400a
林明美 <fxrcode@users.noreply.github.com>
2022-03-25 00:05:27 -0700

    Time: 52 ms (89.80%), Space: 14.3 MB (22.77%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,5 +1,8 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        
+        if len(ransomNote) > len(magazine):
+            return False
         R, M = Counter(ransomNote), Counter(magazine)
         for k, v in R.items():
             if k not in M or M[k] < v:

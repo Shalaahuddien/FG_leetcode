aba212c8b60ebceb03cf2d9ad74ed9d0b3dd374a
林明美 <fxrcode@users.noreply.github.com>
2022-03-25 16:27:40 -0700

    Time: 52 ms (64.93%), Space: 13.9 MB (43.95%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        costs.sort(key=lambda x: x[0] - x[1])
+        n = len(costs) // 2
+        total = 0
+        for i in range(n):
+            total += costs[i][0] + costs[i + n][1]
+        return total

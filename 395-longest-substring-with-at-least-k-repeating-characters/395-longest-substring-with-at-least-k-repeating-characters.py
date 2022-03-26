358ff762c25ddacaf147e249c348797a1087cde2
林明美 <fxrcode@users.noreply.github.com>
2022-03-26 15:07:19 -0700

    Time: 256 ms (23.18%), Space: 13.9 MB (62.87%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.py
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.py
@@ -1,15 +1,30 @@
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
-        @cache
-        def dc(s):
-            if len(s) < k:
-                return 0
-            freq = Counter(s)
-            for i, c in enumerate(s):
-                if freq[c] < k:
-                    sl = dc(s[:i])
-                    sr = dc(s[i + 1 :])
-                    return max(sl, sr)
-            return len(s)
+        def slide(numUniqueTarget):
+            l = r = numUnique = numAtLeastK = count = 0
+            freq = Counter()
+            while r < len(s):
+                c = s[r]
+                if freq[c] == 0:
+                    numUnique += 1
+                freq[c] += 1
+                if freq[c] == k:
+                    numAtLeastK += 1
+                r += 1
 
-        return dc(s)
+                while numUnique > numUniqueTarget:
+                    d = s[l]
+                    if freq[d] == k:
+                        numAtLeastK -= 1
+                    freq[d] -= 1
+                    if freq[d] == 0:
+                        numUnique -= 1
+                    l += 1
+                if numUnique == numAtLeastK:
+                    count = max(count, r - l)
+            return count
+
+        mxcount = 0
+        for i in range(1, 27):
+            mxcount = max(mxcount, slide(i))
+        return mxcount

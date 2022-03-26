82a8c3c32847cd7b2ba56f4b0e6dafa14e2369f6
林明美 <fxrcode@users.noreply.github.com>
2022-03-26 16:35:02 -0700

    Time: 133 ms (71.52%), Space: 15.2 MB (65.07%) - LeetHub

--- 908-smallest-range-i/908-smallest-range-i.py
+++ 908-smallest-range-i/908-smallest-range-i.py
@@ -1,6 +1,4 @@
 class Solution:
     def smallestRangeI(self, nums: List[int], k: int) -> int:
             mn, mx = min(nums), max(nums)
-            if mx - mn > 2 * k:
-                return mx - mn - 2 * k
-            return 0
+            return max(0, mx - mn - 2 * k)

764b42a49548874121f622962114cbeef04f7581
林明美 <fxrcode@users.noreply.github.com>
2022-03-27 18:21:47 -0700

    Time: 60 ms (54.24%), Space: 13.9 MB (81.20%) - LeetHub

--- 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.py
+++ 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.py
@@ -1,11 +1,12 @@
 class Solution:
     def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
-        p_i = list([pieces[i], 0] for i in range(len(pieces)))
+        p_i = list([i, 0] for i in range(len(pieces)))
         last_sublist = -1
         for a in arr:
             if last_sublist == -1:
                 for i, pi in enumerate(p_i):
-                    p, it = pi[0], pi[1]
+                    pidx, it = pi[0], pi[1]
+                    p = pieces[pidx]
                     if it < len(p) and p[it] == a:
                         last_sublist = i
                         it += 1
@@ -18,7 +19,8 @@ class Solution:
                     return False
             else:
                 # in last_sublist
-                p, it = p_i[last_sublist]
+                pidx, it = p_i[last_sublist]
+                p = pieces[pidx]
                 if p[it] != a:
                     return False
                 it += 1

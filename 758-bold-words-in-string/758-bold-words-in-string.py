515552558dc392ed12df3001732bd2afc7170e00
林明美 <fxrcode@users.noreply.github.com>
2022-03-27 22:19:41 -0700

    Time: 50 ms (90.00%), Space: 13.8 MB (94.29%) - LeetHub

--- 758-bold-words-in-string/758-bold-words-in-string.py
+++ 758-bold-words-in-string/758-bold-words-in-string.py
@@ -1,24 +1,35 @@
 class Solution:
     def boldWords(self, words: List[str], s: str) -> str:
-        intervals = []
-        words.sort(key=lambda x: -len(x))
-        for i in range(len(s)):
-            for w in words:
-                if s[i:].startswith(w):
-                    intervals.append([i, i + len(w)])
+        root, n, intervals = {}, len(s), []
+        # create trie
+        for w in words:
+            cur = root
+            for c in w:
+                cur = cur.setdefault(c, {})
+            cur["#"] = 0
 
-        def merge(I):
-            res = []
-            for itv in I:
-                if not res or res[-1][1] < itv[0]:
-                    res.append(itv)
-                else:
-                    res[-1][1] = max(res[-1][1], itv[1])
-            return res
+        # interval merge
+        def merge_interval(iv):
+            if not intervals or intervals[-1][1] < iv[0]:
+                intervals.append(iv)
+            else:
+                intervals[-1][1] = max(intervals[-1][1], iv[1])
 
-        merged = merge(intervals)
+        # make max match and add to interval
+        for i in range(n):
+            cur, mx_end = root, None
+            for j in range(i, n):
+                if s[j] not in cur:
+                    break
+                cur = cur[s[j]]
+                if "#" in cur:
+                    mx_end = j + 1  # jem: [i,j+1)
+            # !only need to add max-match interval!
+            if mx_end:
+                merge_interval([i, mx_end])
+        # concat result
         res, prev_end = [], 0
-        for start, end in merged:
-            res.append(s[prev_end:start] + "<b>" + s[start:end] + "</b>")
-            prev_end = end
+        for l, r in intervals:
+            res.append(s[prev_end:l] + "<b>" + s[l:r] + "</b>")
+            prev_end = r
         return "".join(res + [s[prev_end:]])

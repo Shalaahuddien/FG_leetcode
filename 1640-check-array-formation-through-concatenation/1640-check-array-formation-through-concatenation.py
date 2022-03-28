cf5d0ccba2163b30a8675ec46413617f253ab9af
林明美 <fxrcode@users.noreply.github.com>
2022-03-27 18:43:50 -0700

    Time: 66 ms (42.60%), Space: 13.9 MB (81.20%) - LeetHub

--- 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.py
+++ 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.py
@@ -1,20 +1,14 @@
 class Solution:
     def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
-        pieces.sort(key=lambda x: x[0])
-        p_len = len(pieces)
         n = len(arr)
+        d = {p[0]: pi for pi, p in enumerate(pieces)}
         i = 0
         while i < n:
-            left, right = 0, p_len - 1
-            found = -1
-            # use bisect to find target piece
-            while left < right:
-                mid = (left + right) // 2
-                if pieces[mid][0] >= arr[i]:
-                    right = mid
-                else:
-                    left = mid + 1
-            target_piece = pieces[left]
+            # find target piece
+            if arr[i] not in d:
+                return False
+            pi = d[arr[i]]
+            target_piece = pieces[pi]
             for x in target_piece:
                 if x != arr[i]:
                     return False

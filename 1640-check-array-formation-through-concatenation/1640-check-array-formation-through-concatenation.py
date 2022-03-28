58c64cf837a9743dc003a02ad717b5adbe226fa8
林明美 <fxrcode@users.noreply.github.com>
2022-03-27 18:52:19 -0700

    Time: 59 ms (57.07%), Space: 13.8 MB (81.20%) - LeetHub

--- 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.py
+++ 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.py
@@ -1,16 +1,7 @@
 class Solution:
     def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
-        n = len(arr)
-        d = {p[0]: pi for pi, p in enumerate(pieces)}
-        i = 0
-        while i < n:
-            # find target piece
-            if arr[i] not in d:
-                return False
-            pi = d[arr[i]]
-            target_piece = pieces[pi]
-            for x in target_piece:
-                if x != arr[i]:
-                    return False
-                i += 1
-        return True
+        mp = {p[0]: p for p in pieces}
+        res = []
+        for n in arr:
+            res += mp.get(n, [])
+        return res == arr

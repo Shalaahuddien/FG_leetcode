0309ae7c82c8620f86491d8dbfe5017f7e618b4f
林明美 <fxrcode@users.noreply.github.com>
2022-03-31 00:54:13 -0700

    Time: 27 ms (96.06%), Space: 13.9 MB (64.45%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.py
+++ 1816-truncate-sentence/1816-truncate-sentence.py
@@ -5,5 +5,5 @@ class Solution:
             if s[i] == " ":
                 words += 1
             if words == k:
-                break
-        return s[:i] if i < len(s) - 1 else s
+                return s[:i]
+        return s

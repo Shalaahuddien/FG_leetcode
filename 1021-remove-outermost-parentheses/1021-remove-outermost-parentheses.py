93686c49e177f95f46210d1b6af017afd42fe456
林明美 <fxrcode@users.noreply.github.com>
2022-03-31 22:37:21 -0700

    Time: 62 ms (33.89%), Space: 14 MB (65.53%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -1,14 +1,11 @@
 class Solution:
     def removeOuterParentheses(self, s: str) -> str:
-        res = []
-        count = 0
+        # add every char unless 1st '(' or last ')'
+        res, opened = [], 0
         for c in s:
-            if c == "(":
-                if count != 0:
-                    res.append(c)
-                count += 1
-            else:
-                if count != 1:
-                    res.append(c)
-                count -= 1
+            if c == "(" and opened > 0:
+                res.append(c)
+            if c == ")" and opened > 1:
+                res.append(c)
+            opened += 1 if c == "(" else -1
         return "".join(res)

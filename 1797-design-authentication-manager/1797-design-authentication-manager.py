5d030eee0d0b42f3413c563473b5540ca7b25898
林明美 <fxrcode@users.noreply.github.com>
2022-04-01 22:59:01 -0700

    Time: 203 ms (82.18%), Space: 15.6 MB (30.46%) - LeetHub

--- 1797-design-authentication-manager/1797-design-authentication-manager.py
+++ 1797-design-authentication-manager/1797-design-authentication-manager.py
@@ -1,24 +1,28 @@
 class AuthenticationManager:
 
-    def __init__(self, timeToLive: int):
+    def __init__(self, timeToLive: int) -> None:
         self.ttl = timeToLive
-        self.tokens = {}
+        self.expiry = OrderedDict()
 
     def generate(self, tokenId: str, currentTime: int) -> None:
-        self.tokens[tokenId] = currentTime
+        self.evict_expired(currentTime)
+        self.expiry[tokenId] = self.ttl + currentTime
 
     def renew(self, tokenId: str, currentTime: int) -> None:
-        if tokenId in self.tokens:
-            if self.tokens[tokenId] + self.ttl > currentTime:
-                self.tokens[tokenId] = currentTime
+        self.evict_expired(currentTime)
+        if tokenId in self.expiry:
+            self.expiry.move_to_end(tokenId)
+            self.expiry[tokenId] = self.ttl + currentTime
 
     def countUnexpiredTokens(self, currentTime: int) -> int:
-        cnt = 0
-        for time in self.tokens.values():
-            if time <= currentTime < time + self.ttl:
-                cnt += 1
-        return cnt
-    
+        self.evict_expired(currentTime)
+        return len(self.expiry)
+
+    def evict_expired(self, currentTime: int) -> None:
+        while self.expiry and next(iter(self.expiry.values())) <= currentTime:
+            self.expiry.popitem(last=False)
+            
+            
 # Your AuthenticationManager object will be instantiated and called as such:
 # obj = AuthenticationManager(timeToLive)
 # obj.generate(tokenId,currentTime)

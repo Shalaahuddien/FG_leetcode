66e7d2fe90999c11f2d8f7caa4b3e3baa4bb24c7
林明美 <fxrcode@users.noreply.github.com>
2022-04-02 21:50:10 -0700

    Time: 1352 ms (9.28%), Space: 17.2 MB (8.14%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.py
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.py
@@ -2,18 +2,15 @@ class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
         if k <= 0:
             return 0
-        cnt = 0
-        l, r = 0, 0
-        p = 1
-        while r < len(nums):
-            c = nums[r]
-            r += 1
-            p *= c
-            while l < r and p >= k:
-                d = nums[l]
-                l += 1
-                p //= d
-            # now p < k
-            cnt += r - l
+        k = log(k)
 
-        return cnt
+        prefix = [0]
+        for x in nums:
+            prefix.append(prefix[-1] + log(x))
+
+        ans = 0
+        for i, x in enumerate(prefix):
+            # find max prefix[j] st. prefix[j]-prefix[i] < k
+            j = bisect_right(prefix, x + k - 1e-9, i + 1)
+            ans += j - i - 1
+        return ans

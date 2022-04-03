1651ba728c5a1530b2bb927bf8e02f497465d77b
林明美 <fxrcode@users.noreply.github.com>
2022-04-02 19:05:57 -0700

    Time: 115 ms (86.28%), Space: 14.7 MB (19.91%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -3,7 +3,8 @@ class Solution:
         l,r = 0, len(s)-1
         while l < r:
             if s[l] != s[r]:
-                one,two = s[l+1:r+1], s[l:r]
+                one = s[l:r]
+                two = s[l+1:r+1]
                 return one == one[::-1] or two == two[::-1]
             l,r = l+1,r-1
         return True

0d7fa8b0646e90a17973e5653e8ea567154897ba
林明美 <fxrcode@users.noreply.github.com>
2022-04-03 22:05:48 -0700

    Time: 3411 ms (5.05%), Space: 24.6 MB (59.49%) - LeetHub

--- 1648-sell-diminishing-valued-colored-balls/1648-sell-diminishing-valued-colored-balls.py
+++ 1648-sell-diminishing-valued-colored-balls/1648-sell-diminishing-valued-colored-balls.py
@@ -1,34 +1,15 @@
 class Solution:
     def maxProfit(self, inventory: List[int], orders: int) -> int:
-        MOD = 10**9 + 7
-        A = inventory
-        A.sort()
-        A = [0] + A
-        r = len(A) - 1
-        gain = 0
+        fn = lambda x: sum(max(0, xx - x) for xx in inventory)  # balls sold
 
-        def sm(v, num):
-            last, fir = v, v - num + 1
-            return (fir + last) * num // 2
+        # last true binary search
+        lo, hi = 0, max(inventory)
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if fn(mid) < orders:  # FFFFTTT
+                hi = mid
+            else:
+                lo = mid + 1
 
-        while orders:
-            less_idx = bisect_left(A, A[r]) - 1
-            less = A[less_idx]
-            batch = r - less_idx
-            diff = A[r] - less
-            bought = min(orders, diff * batch)
-            num_batch, last_batch = divmod(bought, batch)
-            if num_batch:
-                for i in range(less_idx + 1, r + 1):
-                    gain += sm(A[i], num_batch)
-                    A[i] -= num_batch
-            for i in range(less_idx + 1, less_idx + 1 + last_batch):
-                gain += A[i]
-                A[i] -= 1
-            orders -= bought
-            # for i in range(less_idx + 1, r + 1):
-            #     if orders != 0 and A[i] > less:
-            #         gain += A[i]
-            #         A[i] -= 1
-            #         orders -= 1
-        return gain % MOD
+        ans = sum((x + lo + 1) * (x - lo) // 2 for x in inventory if x > lo)
+        return (ans - (fn(lo) - orders) * (lo)) % 1_000_000_007

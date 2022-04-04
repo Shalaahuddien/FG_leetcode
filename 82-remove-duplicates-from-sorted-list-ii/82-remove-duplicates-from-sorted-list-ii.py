86c1e7c5ecf81e18034bfbed05f22b0c712a6bd5
林明美 <fxrcode@users.noreply.github.com>
2022-04-04 00:27:55 -0700

    Time: 82 ms (12.52%), Space: 13.8 MB (77.57%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
@@ -5,27 +5,15 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: ListNode) -> ListNode:
-        # sentinel
-        sentinel = ListNode(0, head)
-
-        # predecessor = the last node 
-        # before the sublist of duplicates
-        pred = sentinel
-        
-        while head:
-            # if it's a beginning of duplicates sublist 
-            # skip all duplicates
-            if head.next and head.val == head.next.val:
-                # move till the end of duplicates sublist
-                while head.next and head.val == head.next.val:
-                    head = head.next
-                # skip all duplicates
-                pred.next = head.next 
-            # otherwise, move predecessor
+        dummy = ListNode(None, head)
+        pre, cur = dummy, head
+        while cur:
+            if cur.next and cur.val == cur.next.val:
+                val_to_rm = cur.val
+                while cur and cur.val == val_to_rm:
+                    cur = cur.next
+                # not cur or cur is start non-dup sublist
+                pre.next = cur
             else:
-                pred = pred.next 
-                
-            # move forward
-            head = head.next
-            
-        return sentinel.next
+                pre, cur = cur, cur.next
+        return dummy.next

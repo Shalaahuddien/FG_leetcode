384472b6a4d4e5bd82530fd740fd3faa9763a774
林明美 <fxrcode@users.noreply.github.com>
2022-04-04 19:09:03 -0700

    Time: 709 ms (56.66%), Space: 40.5 MB (91.87%) - LeetHub

--- 1845-seat-reservation-manager/1845-seat-reservation-manager.py
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.py
@@ -2,7 +2,6 @@ class SeatManager:
 
     def __init__(self, n: int):
         self.mnpq = list(range(1, n + 1))
-        heapify(self.mnpq)
 
     def reserve(self) -> int:
         return heappop(self.mnpq)

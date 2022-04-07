d1a131d98df4b099f62fc8d0aa9c7244467998b9
林明美 <fxrcode@users.noreply.github.com>
2022-04-07 11:15:50 -0700

    Time: 888 ms (81.58%), Space: 136.9 MB (58.45%) - LeetHub

--- 1057-campus-bikes/1057-campus-bikes.py
+++ 1057-campus-bikes/1057-campus-bikes.py
@@ -1,20 +1,28 @@
 class Solution:
     def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) -> List[int]:
-        distances = []
-        for i, (x, y) in enumerate(workers):
-            for j, (u, v) in enumerate(bikes):
-                d = abs(x - u) + abs(y - v)
-                distances.append((d, i, j))
-
-        distances.sort()
-
+        w2b_list = []
         used_bikes = set()
-        result = [-1] * len(workers)
-        for dist, i, j in distances:
-            if len(used_bikes) == len(workers):
+        ans = [-1] * len(workers)
+        pq = []
+        for i, (x,y) in enumerate(workers):
+            cur_wb_pairs = []
+            for j, (u,v) in enumerate(bikes):
+                d = abs(x-u) + abs(y-v)
+                cur_wb_pairs.append((d, i,j))
+            cur_wb_pairs.sort(reverse=True)
+            heappush(pq, cur_wb_pairs.pop())
+            w2b_list.append(cur_wb_pairs)
+            
+        while pq:
+            if len(used_bikes) == len(bikes):
                 break
-            # print(dist, i, j)
-            if result[i] == -1 and j not in used_bikes:
-                used_bikes.add(j)
-                result[i] = j
-        return result
+            d,w,b = heappop(pq)
+            if ans[w] == -1 and b not in used_bikes:
+                used_bikes.add(b)
+                ans[w] = b
+            else:
+                next_closet_bike = w2b_list[w].pop()
+                heappush(pq, next_closet_bike)
+                
+        return ans
+            

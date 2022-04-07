ced91286485b31a84ce694fdca522483e04a088a
林明美 <fxrcode@users.noreply.github.com>
2022-04-07 11:55:44 -0700

    Time: 68 ms (5.94%), Space: 13.9 MB (62.72%) - LeetHub

--- 362-design-hit-counter/362-design-hit-counter.py
+++ 362-design-hit-counter/362-design-hit-counter.py
@@ -1,29 +1,24 @@
 class HitCounter:
-
     def __init__(self):
-        self.wind = deque()
-
-    def _pop_old(self, ts):
-        while self.wind:
-            if ts - 300 + 1 > self.wind[0]:
-                self.wind.popleft()
-            else:
-                break
+        self.q = deque()
+        self.total = 0
 
     def hit(self, timestamp: int) -> None:
-        self.wind.append(timestamp)
-        self._pop_old(timestamp)
+        if self.q and self.q[-1][0] == timestamp:
+            self.q[-1][1] += 1
+        else:
+            self.q.append([timestamp, 1])
+        self.total += 1
 
     def getHits(self, timestamp: int) -> int:
-        self._pop_old(timestamp)
-        lo, hi = timestamp - 300 + 1, timestamp
-        """
-        range_count_query (common pattern of bisect usage)
-        count(lo...hi) = count(<=hi) - count(<lo)
-        reminds me of slide-window exact(k) = atMost(k) - atMost(k-1)
-        """
-        return bisect_right(self.wind, hi) - bisect_left(self.wind, lo)
-
+        while self.q:
+            diff = timestamp - self.q[0][0]
+            if diff >= 300:
+                x = self.q.popleft()
+                self.total -= x[1]
+            else:
+                break
+        return self.total
 
 
 # Your HitCounter object will be instantiated and called as such:

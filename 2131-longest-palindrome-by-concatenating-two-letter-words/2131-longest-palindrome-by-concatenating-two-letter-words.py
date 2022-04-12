04569946cddf6ff4f1da85525a345d4cf5066753
林明美 <fxrcode@users.noreply.github.com>
2022-04-12 11:09:12 -0700

    Time: 1400 ms (85.18%), Space: 38.4 MB (26.12%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,23 +1,15 @@
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
-        dif = defaultdict(lambda: [0, 0])
-        sym = Counter()
+        counter, ans = [[0] * 26 for _ in range(26)], 0
         for w in words:
-            if w != w[::-1]:
-                signt = "".join(sorted(list(w)))
-                if w == signt:
-                    dif[signt][0] += 1
-                else:
-                    dif[signt][1] += 1
+            a, b = ord(w[0]) - ord("a"), ord(w[1]) - ord("a")
+            if counter[b][a]:
+                ans += 4
+                counter[b][a] -= 1
             else:
-                sym[w] += 1
-        cnt_d = cnt_s = 0
-        for w in dif:
-            cnt_d += min(dif[w])
-        odd = False
-        for w in sym:
-            if sym[w] % 2:
-                odd = True
-            cnt_s += sym[w] // 2
-        
-        return (cnt_d + cnt_s) * 4 + odd * 2
+                counter[a][b] += 1
+        for i in range(26):
+            if counter[i][i]:
+                ans += 2
+                break
+        return ans

23e046c610646f68f9d7fb8675c2d6169fecb985
林明美 <fxrcode@users.noreply.github.com>
2022-04-12 02:19:06 -0700

    Time: 36 ms (85.68%), Space: 14.1 MB (12.69%) - LeetHub

--- 289-game-of-life/289-game-of-life.py
+++ 289-game-of-life/289-game-of-life.py
@@ -3,36 +3,22 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        def cnt(i, j):
-            ones = 0
-            for dx in range(-1, 2):
-                for dy in range(-1, 2):
-                    x, y = i + dx, j + dy
-                    if (x, y) == (i, j):
-                        continue
-                    if 0 <= x < m and 0 <= y < n and abs(board[x][y]) == 1:
-                        ones += 1
-            return ones
-
         m, n = len(board), len(board[0])
-        for i in range(m):
-            for j in range(n):
-                ones = cnt(i, j)
-                if board[i][j] == 1:
-                    if ones < 2:
-                        board[i][j] = -1
-                    elif 2 <= ones <= 3:
-                        board[i][j] = 1
-                    else:
-                        board[i][j] = -1
-                else:
-                    if ones == 3:
-                        board[i][j] = 2
 
+        def cntNei(i, j):
+            return sum(1 for x in (i - 1, i, i + 1) for y in (j - 1, j, j + 1) if 0 <= x < m and 0 <= y < n and board[x][y] & 1)
+
+        # count neighbors
         for i in range(m):
             for j in range(n):
-                if board[i][j] == -1:
-                    board[i][j] = 0
-                if board[i][j] == 2:
-                    board[i][j] = 1
-        return board
+                cnt = cntNei(i, j)
+                cnt -= board[i][j]  # because we included the cell in the neighbors count
+                print(i, j, cnt)
+                if cnt == 3 or (cnt == 2 and board[i][j] == 1):  # live conditions
+                    board[i][j] |= 2  # equivalent to board[i][j] |= 1<<1 --- set the 2nd bit which is the next set
+
+        # convert back
+        for i in range(m):
+            for j in range(n):
+                board[i][j] >>= 1
+        print(board)

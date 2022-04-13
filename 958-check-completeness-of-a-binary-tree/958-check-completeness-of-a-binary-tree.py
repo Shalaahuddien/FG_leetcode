d6e792fb904738a345b58fdad74c8a690dd46d46
林明美 <fxrcode@users.noreply.github.com>
2022-04-13 13:43:51 -0700

    Time: 23 ms (99.94%), Space: 13.9 MB (73.56%) - LeetHub

--- 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.py
+++ 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.py
@@ -6,16 +6,16 @@
 #         self.right = right
 class Solution:
     def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
-        have_none = False
         q = deque([root])
+        pre = root
         while q:
+            # have_none = False # WA: [1,2,3,4,5,6,7,8,9,10,11,12,13,null,null,15]
             for _ in range(len(q)):
                 cur = q.popleft()
-                if not cur:
-                    have_none = True
-                    continue
-                if have_none:
-                    return False
-                q.append(cur.left)
-                q.append(cur.right)
+                if cur:
+                    if not pre:
+                        return False
+                    q.append(cur.left)
+                    q.append(cur.right)
+                pre = cur
         return True

e0006de296dc9f87718c2a0b93a8b42a921e94df
林明美 <fxrcode@users.noreply.github.com>
2022-04-16 00:39:23 -0700

    Time: 893 ms (97.36%), Space: 33.1 MB (88.75%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         N = len(nums)
-        pre = [0] * (N + 1)
+        pre = 0
         pre_r = {0: -1}
         for i in range(N):
-            pre[i + 1] = pre[i] + nums[i]
-            remainder = pre[i + 1] % k
+            pre = pre + nums[i]
+            remainder = pre % k
             if i - pre_r.setdefault(remainder, i) >= 2:
                 return True
         return False

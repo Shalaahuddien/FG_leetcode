471710eaaafcfa17b0bfdbddb2e0cce62f1a2cf6
林明美 <fxrcode@users.noreply.github.com>
2022-04-18 13:28:16 -0700

    Time: 209 ms (67.01%), Space: 16.9 MB (40.44%) - LeetHub

--- 529-minesweeper/529-minesweeper.py
+++ 529-minesweeper/529-minesweeper.py
@@ -1,40 +1,32 @@
 class Solution:
     def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
         m, n = len(board), len(board[0])
-        nums = [[0] * n for _ in range(m)]
-        for i in range(m):
-            for j in range(n):
-                if board[i][j].isdigit():
-                    nums[i][j] = board[i][j]
-                if board[i][j] == "M":
-                    for dx in range(-1, 2):
-                        for dy in range(-1, 2):
-                            ni, nj = i + dx, j + dy
-                            if not (0 <= ni < m and 0 <= nj < n):
-                                continue
-                            if board[ni][nj] == "E":
-                                nums[ni][nj] = str(int(nums[ni][nj]) + 1)
-        vis = set()
+        def valid(i, j):
+            return 0 <= i < m and 0 <= j < n
 
-        def dfs(i, j):
-            if board[i][j] == "M":
-                board[i][j] = "X"
+        def reveal(i, j):
+            # board is: M, E, B, 1~8, X
+            if not valid(i, j) or board[i][j] != "E":
                 return
-            if nums[i][j] != 0:
-                board[i][j] = nums[i][j]
-                return
-            board[i][j] = "B"
-            vis.add((i, j))
+            # count (i,j)'s adjacent mines
+            mines = 0
             for dx in range(-1, 2):
                 for dy in range(-1, 2):
                     ni, nj = i + dx, j + dy
-                    if not (0 <= ni < m and 0 <= nj < n):
-                        continue
-                    if (ni, nj) in vis:
-                        continue
-                    if board[ni][nj] == "M":
-                        continue
-                    dfs(ni, nj)
+                    if valid(ni, nj) and board[ni][nj] == "M":
+                        mines += 1
+            if mines:
+                board[i][j] = str(mines)
+                return
+            else:
+                board[i][j] = "B"
+                for dx in range(-1, 2):
+                    for dy in range(-1, 2):
+                        reveal(i + dx, j + dy)
 
-        dfs(click[0], click[1])
+        x, y = click
+        if board[x][y] == "M":
+            board[x][y] = "X"
+        elif board[x][y] == "E":
+            reveal(x, y)
         return board

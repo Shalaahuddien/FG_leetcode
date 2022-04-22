7f5e6c266ba25db7f340ca47bc04ea5739417caa
林明美 <fxrcode@users.noreply.github.com>
2022-04-22 10:11:27 -0700

    Time: 214 ms (76.20%), Space: 19.4 MB (49.77%) - LeetHub

--- 705-design-hashset/705-design-hashset.py
+++ 705-design-hashset/705-design-hashset.py
@@ -1,32 +1,33 @@
 class MyHashSet:
 
     def __init__(self):
-        self.base = 799
-        self.hash_set = [[] for i in range(self.base)]
-    
-    def hash_key(self, key: int):
-        return key%self.base
-    
+        self.hsize = 1999
+        # self.hsize = int(1e6)  # XXX; all scientific notation num are float!
+
+        self.rad = 33
+        self.buckets = [[] for _ in range(self.hsize)]
+
+    def _hash(self, n):
+        d, q = self.rad, self.hsize
+        h = 0
+        while n:
+            n, v = divmod(n, 10)
+            h = (h * d + v) % q
+        return h
+
     def add(self, key: int) -> None:
-        hash_pos = self.hash_key(key)
-        if key in self.hash_set[hash_pos]:
-            pass
-        else:
-            self.hash_set[hash_pos].append(key)
+        h = self._hash(key)
+        if not key in self.buckets[h]:
+            self.buckets[h].append(key)
 
     def remove(self, key: int) -> None:
-        hash_pos = self.hash_key(key)
-        if key not in self.hash_set[hash_pos]:
-            pass
-        else:
-            self.hash_set[hash_pos].remove(key)
+        h = self._hash(key)
+        if key in self.buckets[h]:
+            self.buckets[h].remove(key)
 
     def contains(self, key: int) -> bool:
-        hash_pos = self.hash_key(key)
-        if key in self.hash_set[hash_pos]:
-            return True
-        else:
-            return False
+        h = self._hash(key)
+        return key in self.buckets[h]
 
 
 # Your MyHashSet object will be instantiated and called as such:

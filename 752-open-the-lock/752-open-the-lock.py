2361388f3ec0864022acd3dac75a28c321060754
林明美 <fxrcode@users.noreply.github.com>
2022-04-22 13:26:26 -0700

    Time: 110 ms (99.85%), Space: 14.4 MB (98.67%) - LeetHub

--- 752-open-the-lock/752-open-the-lock.py
+++ 752-open-the-lock/752-open-the-lock.py
@@ -1,19 +1,27 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
         dead = set(deadends)
-        q = deque([(0, "0000")])
         if "0000" in dead:
             return -1
-        while q:
-            steps, code = q.popleft()
-            if code == target:
-                return steps
-
-            for i in range(4):
-                d = int(code[i])
-                for k in (d - 1) % 10, (d + 1) % 10:
-                    cand = code[:i] + str(k) + code[i + 1 :]
-                    if cand not in dead:
-                        dead.add(cand)
-                        q.append((steps + 1, cand))
+        q1, q2 = set(["0000"]), set([target])
+        vis = set(["0000", target, *deadends])
+        ops = 0
+        while q1:
+            if len(q1) > len(q2):
+                q1, q2 = q2, q1
+            q1_nxt = set()
+            for code in q1:
+                if code in q2:
+                    return ops
+                for i in range(4):
+                    d = int(code[i])
+                    for k in (d - 1) % 10, (d + 1) % 10:
+                        cand = code[:i] + str(k) + code[i + 1 :]
+                        if cand in q2:
+                            return ops + 1
+                        if cand not in vis:
+                            vis.add(cand)
+                            q1_nxt.add(cand)
+            q1 = q1_nxt
+            ops += 1
         return -1

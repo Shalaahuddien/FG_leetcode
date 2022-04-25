88b2b4d60e206954350f1abc4262a69dbe6c92c1
林明美 <fxrcode@users.noreply.github.com>
2022-04-24 19:34:49 -0700

    Time: 38 ms (61.50%), Space: 14 MB (20.03%) - LeetHub

--- 500-keyboard-row/500-keyboard-row.py
+++ 500-keyboard-row/500-keyboard-row.py
@@ -3,9 +3,7 @@ class Solution:
         one, two, three = set("qwertyuiop"), set("asdfghjkl"), set("zxcvbnm")
         res = []
         for w in words:
-            for s in (one, two, three):
-                if w[0].lower() in s:
-                    break
-            if all(c.lower() in s for c in w[1:]):
+            ws = set(w.lower())
+            if ws <= one or ws <= two or ws <= three:
                 res.append(w)
         return res

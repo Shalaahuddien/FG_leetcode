c3b83f7bc14dab0d3e19702153d5d9bdb975afe2
林明美 <fxrcode@users.noreply.github.com>
2022-04-26 14:20:11 -0700

    Time: 88 ms (84.58%), Space: 15.5 MB (82.73%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.py
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -1,24 +1,11 @@
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
-        x, y = abs(x), abs(y)
-        q1, q2 = set([(0, 0)]), set([(x, y)])
-        step = 0
-        vis = set([(0, 0), (x, y)])
-        DIR = [(1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-2, 1), (-1, 2)]
-        while q1:
-            if len(q1) > len(q2):
-                q1, q2 = q2, q1
-            q1_nxt = set()
-            for i, j in q1:
-                if (i, j) in q2:
-                    return step
-                for dx, dy in DIR:
-                    ii, jj = i + dx, j + dy
-                    if (ii, jj) in q2:
-                        return step + 1
-                    if (ii, jj) not in vis and -1 <= ii <= x + 2 and -1 <= jj <= y + 2:
-                        vis.add((ii, jj))
-                        q1_nxt.add((ii, jj))
-            q1 = q1_nxt
-            step += 1
-        return -1
+        @cache
+        def dp(x, y):
+            if x + y == 0:
+                return 0
+            elif x + y == 2:
+                return 2
+            return min(dp(abs(x - 1), abs(y - 2)), dp(abs(x - 2), abs(y - 1))) + 1
+
+        return dp(abs(x), abs(y))

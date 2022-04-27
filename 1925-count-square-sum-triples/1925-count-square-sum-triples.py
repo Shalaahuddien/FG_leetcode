ed98224ad73f1490d87558dc261ed899302e2a00
林明美 <fxrcode@users.noreply.github.com>
2022-04-27 16:29:22 -0700

    Time: 428 ms (61.22%), Space: 13.8 MB (96.77%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.py
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -3,8 +3,8 @@ class Solution:
         ans = 0
         for a in range(3, n + 1):
             for b in range(a + 1, n + 1):
-                c2 = a**2 + b**2
-                c = isqrt(c2)
-                if c**2 == c2 and c2 <= n**2:
+                c2 = a * a + b * b
+                c = sqrt(c2)
+                if int(c) == c and c2 <= n * n:
                     ans += 2
         return ans

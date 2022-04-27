7b33f4264125602117d153cbd7a3ee53fd94b9dc
林明美 <fxrcode@users.noreply.github.com>
2022-04-27 11:46:38 -0700

    Time: 59 ms (29.72%), Space: 13.9 MB (36.87%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.py
@@ -1,12 +1,4 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
-        VOWELS = set("aeiouAEIOU")
-
-        def vows(ss):
-            cnt = 0
-            for c in ss:
-                if c in VOWELS:
-                    cnt += 1
-            return cnt
-
-        return vows(s[: len(s) // 2]) == vows(s[len(s) // 2 :])
+        s, n, VOWS = s.lower(), len(s), set("aeiou")
+        return sum(c in VOWS for c in s[: n // 2]) == sum(c in VOWS for c in s[n // 2 :])

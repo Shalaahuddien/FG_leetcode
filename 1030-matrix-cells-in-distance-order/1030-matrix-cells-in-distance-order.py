3a3b991bc95d7897ebcc08f9b290f88055c88e29
林明美 <fxrcode@users.noreply.github.com>
2022-04-27 17:31:58 -0700

    Time: 156 ms (97.54%), Space: 15.8 MB (94.09%) - LeetHub

--- 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.py
+++ 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.py
@@ -1,14 +1,5 @@
 class Solution:
     def allCellsDistOrder(self, rows: int, cols: int, rCenter: int, cCenter: int) -> List[List[int]]:
-        res = set()
-        for d in range(rows - 1 + cols - 1 + 1):
-            for dx in range(d + 1):
-                dy = d - dx
-                for sx, sy in ((-1, -1), (-1, 1), (1, -1), (1, 1)):
-                    nx, ny = rCenter + sx * dx, cCenter + sy * dy
-                    if 0 <= nx < rows and 0 <= ny < cols:
-                        res.add((nx, ny))
-        sres = list(res)
-        sres.sort(key=lambda x: (abs(x[0] - rCenter) + abs(x[1] - cCenter)))
-        return sres
-        return list(res)
+        res = [(r, c) for r in range(rows) for c in range(cols)]
+        res.sort(key=lambda cell: abs(cell[0] - rCenter) + abs(cell[1] - cCenter))
+        return res

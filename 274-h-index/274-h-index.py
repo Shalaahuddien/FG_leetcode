cecd71b6174acd1e5703e5cdb1380550e6e9c9e7
恒 <fxrcode@users.noreply.github.com>
2022-04-27 18:48:39 -0700

    Time: 48 ms (59.68%), Space: 14.1 MB (73.56%) - LeetHub

--- 274-h-index/274-h-index.py
+++ 274-h-index/274-h-index.py
@@ -1,10 +1,8 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         citations.sort(reverse=True)
-        h = 0
-        for i, c in enumerate(citations):
-            if c > i:
-                h += 1
-            else:
-                break
-        return h
+        i = 0
+        while i < len(citations) and citations[i] > i:
+            i += 1
+        # now we got i paper has citations greater or equal to i.
+        return i

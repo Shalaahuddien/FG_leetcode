7ec8d6e542f484d78e29adfc8aadace3b3d3224b
林明美 <fxrcode@users.noreply.github.com>
2022-04-27 17:45:17 -0700

    Time: 415 ms (5.67%), Space: 28.8 MB (6.40%) - LeetHub

--- 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.py
+++ 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.py
@@ -1,14 +1,12 @@
 class Solution:
     def allCellsDistOrder(self, rows: int, cols: int, rCenter: int, cCenter: int) -> List[List[int]]:
-        q, res = deque([(rCenter, cCenter)]), []
-        vis = set(q)
-        while q:
-            for _ in range(len(q)):
-                r, c = q.popleft()
-                res.append((r, c))
-                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                    xx, yy = r + dx, c + dy
-                    if 0 <= xx < rows and 0 <= yy < cols and (xx, yy) not in vis:
-                        vis.add((xx, yy))
-                        q.append((xx, yy))
+        def dfs(i, j):
+            vis.add((i, j))
+            res.append((i, j))
+            for x, y in (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1):
+                if 0 <= x < rows and 0 <= y < cols and (x, y) not in vis:
+                    dfs(x, y)
+        res, vis = [], set()
+        dfs(rCenter, cCenter)
+        res.sort(key=lambda x: abs(x[0] - rCenter) + abs(x[1] - cCenter))
         return res

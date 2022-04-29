41643d572a98f23a1a298875c8b2dd6b688eab95
fxrc <fxrcode@users.noreply.github.com>
2022-04-29 11:28:17 -0700

    Time: 5816 ms (5.47%), Space: 24.4 MB (5.11%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,22 +1,24 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        INF = 2e9
         DIR = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
         m, n = len(heights), len(heights[0])
-        dist = [[INF] * n for _ in range(m)]
-        src, dst = (0, 0), (m - 1, n - 1)
-        pq = [(0, src)]  # dist, row/col
-        while pq:
-            d, (r, c) = heappop(pq)
-            if d > dist[r][c]:
-                continue
-            if (r, c) == dst:
-                return d
-            for i in range(4):
-                xx, yy = r + DIR[i][0], c + DIR[i][1]
-                if not (0 <= xx < m and 0 <= yy < n):
-                    continue
-                new_dist = max(d, abs(heights[xx][yy] - heights[r][c]))
-                if dist[xx][yy] > new_dist:
-                    dist[xx][yy] = new_dist
-                    heappush(pq, (new_dist, (xx, yy)))
+
+        def dfs(LIMIT, i, j):
+            vis.add((i, j))
+            for dx, dy in DIR:
+                x, y = i + dx, j + dy
+                if 0 <= x < m and 0 <= y < n and (x, y) not in vis:
+                    if abs(heights[i][j] - heights[x][y]) <= LIMIT:
+                        dfs(LIMIT, x, y)
+
+        l, r = 0, max(max(heights, key=max))
+        while l < r:
+            mid = (l + r) // 2
+            vis = set()
+            dfs(mid, 0, 0)
+            if (m - 1, n - 1) in vis:
+                r = mid
+            else:
+                l = mid + 1
+        return l

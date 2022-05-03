2f34240ea8fe81cc5fe169a3382f139d3f0f25d7
fxrc <fxrcode@users.noreply.github.com>
2022-05-03 00:40:09 -0700

    Time: 56 ms (60.36%), Space: 13.9 MB (19.38%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -1,11 +1,9 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        q = deque([[t, i] for i, t in enumerate(tickets)])
-        sec = 0
-        while q:
-            t, i = q.popleft()
-            if i == k and t == 1:
-                return sec + 1
-            q.append([t - 1 if t > 0 else 0, i])
-            if t != 0:
-                sec += 1
+        secs = 0
+        for i, t in enumerate(tickets):
+            if i <= k:
+                secs += min(t, tickets[k])
+            else:
+                secs += min(t, tickets[k] - 1)
+        return secs

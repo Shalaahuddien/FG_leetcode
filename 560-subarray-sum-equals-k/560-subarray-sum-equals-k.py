0137cdd55656e582863884f9f4a564a2437d3650
fxrc <fxrcode@users.noreply.github.com>
2022-05-02 22:02:07 -0700

    Time: 312 ms (57.13%), Space: 16.6 MB (33.36%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -1,13 +1,8 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        P = 0
-        D = defaultdict(int)
-        D[0] += 1
-        ans = 0
-        for i,n in enumerate(nums):
-            P += n
-            pre = P - k
-            if pre in D:
-                ans += D[pre]
-            D[P] += 1
-        return ans
+        cnt, cur, res = {0:1}, 0, 0
+        for n in nums:
+            cur +=n 
+            res += cnt.get(cur-k, 0) 
+            cnt[cur] = cnt.get(cur, 0)+1
+        return res

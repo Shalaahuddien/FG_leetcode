21179186c040a47c7758ec8d681b65f3fff5307e
fxrc.cs <fxrcode@users.noreply.github.com>
2022-05-07 13:35:27 -0700

    Time: 469 ms (47.77%), Space: 32.1 MB (33.55%) - LeetHub

--- 456-132-pattern/456-132-pattern.py
+++ 456-132-pattern/456-132-pattern.py
@@ -1,16 +1,11 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        N = len(nums)
-        leftMin = [float('inf')]*N
-        for i in range(1, N):
-            leftMin[i] = min(leftMin[i-1], nums[i-1])
-        stk =[]
-        for j in range(N-1,-1,-1):
-            numsk = float('-inf')
-            while stk and stk[-1] < nums[j]:
-                numsk= stk.pop()
-            if leftMin[j] < numsk:
+        k = -2e9
+        stk = []
+        for i in range(len(nums)-1, -1, -1):
+            if nums[i] < k:
                 return True
-            stk.append(nums[j])
+            while stk and stk[-1] < nums[i]:
+                k = max(k, stk.pop())
+            stk.append(nums[i])
         return False
-        

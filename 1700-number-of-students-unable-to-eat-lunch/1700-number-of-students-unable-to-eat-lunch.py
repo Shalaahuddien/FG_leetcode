b70bbcd23124bbd748d4f02a24d8b2914c891db4
fxrc.cs <fxrcode@users.noreply.github.com>
2022-05-07 21:18:54 -0700

    Time: 63 ms (29.46%), Space: 13.8 MB (98.31%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,14 +1,8 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        students, sandwiches = deque(students), deque(sandwiches)
-        while True:
-            l = len(students)
-            for _ in range(l):
-                if students[0] == sandwiches[0]:
-                    students.popleft()
-                    sandwiches.popleft()
-                else:
-                    students.append(students.popleft())
-            if l == len(students):
-                break
-        return len(students)
+        cnt = Counter(students)
+        n,k = len(students), 0
+        while k < n and cnt[sandwiches[k]]:
+            cnt[sandwiches[k]] -= 1
+            k += 1
+        return n-k

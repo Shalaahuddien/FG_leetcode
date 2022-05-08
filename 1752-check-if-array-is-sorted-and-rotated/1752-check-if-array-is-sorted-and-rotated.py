9e630b27fbd524c65a857dd956d8aa0a6df46341
fxrc.cs <fxrcode@users.noreply.github.com>
2022-05-07 21:44:44 -0700

    Time: 62 ms (18.83%), Space: 13.8 MB (96.69%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -1,12 +1,9 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        mn = nums[0]
-        for i in range(len(nums) - 1):
-            if not nums[i] <= nums[i + 1]:
-                break
-        else:
-            return True
-        for j in range(i + 1, len(nums)):
-            if nums[j] > mn or j + 1 < len(nums) and nums[j] > nums[j + 1]:
+        k,n = 0, len(nums)
+        for i in range(n):
+            if nums[i] > nums[(i+1)%n]:
+                k += 1
+            if k > 1:
                 return False
         return True

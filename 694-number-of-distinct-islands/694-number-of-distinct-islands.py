5e69c05fb758d34ff927e2dbde6c184b0ccbc662
fxrc.cs <fxrcode@users.noreply.github.com>
2022-05-15 17:45:19 -0700

    Time: 303 ms (41.78%), Space: 17.8 MB (32.82%) - LeetHub

--- 694-number-of-distinct-islands/694-number-of-distinct-islands.py
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.py
@@ -1,19 +1,19 @@
 class Solution:
     def numDistinctIslands(self, grid: List[List[int]]) -> int:
-        m,n = len(grid), len(grid[0])
+        m, n = len(grid), len(grid[0])
         # dfs to find all cells in the current island
-        def dfs(i,j,d):
-            if not (0 <= i < m and 0 <=j<n):
+        def dfs(i, j, d):
+            if not (0 <= i < m and 0 <= j < n):
                 return
-            if (i,j) in vis or not grid[i][j]:
+            if (i, j) in vis or not grid[i][j]:
                 return
-            vis.add((i,j))
+            vis.add((i, j))
             path_signature.append(d)
-            dfs(i+1,j,'D')
-            dfs(i-1,j,'U')
-            dfs(i,j+1,'R')
-            dfs(i,j-1,'L')
-            path_signature.append('0')
+            dfs(i + 1, j, "D")
+            dfs(i - 1, j, "U")
+            dfs(i, j + 1, "R")
+            dfs(i, j - 1, "L")
+            path_signature.append("x")
 
         # repeatedly start DFS as long as there're islands remaining
         vis = set()
@@ -21,8 +21,8 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 path_signature = []
-                dfs(i,j,'0')
+                dfs(i, j, "")
                 if path_signature:
-                    unique_islands.add(''.join(path_signature))
+                    unique_islands.add("".join(path_signature))
         # print(unique_islands)
         return len(unique_islands)

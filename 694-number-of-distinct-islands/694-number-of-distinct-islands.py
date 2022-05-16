5ee6e0da32ad05234bb8349d4cc584cb27da2540
fxrc.cs <fxrcode@users.noreply.github.com>
2022-05-15 17:26:19 -0700

    Time: 298 ms (43.33%), Space: 18 MB (28.50%) - LeetHub

---
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numDistinctIslands(self, grid: List[List[int]]) -> int:
+        m,n = len(grid), len(grid[0])
+        # dfs to find all cells in the current island
+        def dfs(i,j,d):
+            if not (0 <= i < m and 0 <=j<n):
+                return
+            if (i,j) in vis or not grid[i][j]:
+                return
+            vis.add((i,j))
+            path_signature.append(d)
+            dfs(i+1,j,'D')
+            dfs(i-1,j,'U')
+            dfs(i,j+1,'R')
+            dfs(i,j-1,'L')
+            path_signature.append('0')
+
+        # repeatedly start DFS as long as there're islands remaining
+        vis = set()
+        unique_islands = set()
+        for i in range(m):
+            for j in range(n):
+                path_signature = []
+                dfs(i,j,'0')
+                if path_signature:
+                    unique_islands.add(''.join(path_signature))
+        # print(unique_islands)
+        return len(unique_islands)

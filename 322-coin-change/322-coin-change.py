cd7b2487b79c9e46d5b46168ef2a66f36edd2b2a
fxrc.cs <fxrcode@users.noreply.github.com>
2022-05-22 14:20:39 -0700

    Time: 2159 ms (37.65%), Space: 30.8 MB (13.13%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,21 +1,15 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        q = deque([0])
-        vis = set(q)
-        coins.sort()
-        step = 0
-        while q:
-            for _ in range(len(q)):
-                u = q.popleft()
-                if u == amount:
-                    return step
-                for c in coins:
-                    v = u + c
-                    if v in vis:
-                        continue
-                    if v > amount:
-                        break
-                    vis.add(v)
-                    q.append(v)
-            step += 1
-        return -1
+        INF = float("inf")
+
+        @cache
+        def dp(i):
+            if i == 0:
+                return 0
+            if i < 0:
+                # BUG: return 2e9
+                # XXX: INF+anything => INF.
+                return INF
+            return min(dp(i - c) + 1 for c in coins)
+
+        return dp(amount) if dp(amount) != INF else -1

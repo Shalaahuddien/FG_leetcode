646e4d9bb6908d6031a3b580248cca172a34d083
fxrc.cs <fxrcode@users.noreply.github.com>
2022-05-22 13:56:57 -0700

    Time: 1890 ms (50.85%), Space: 14.2 MB (60.09%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.py
+++ 277-find-the-celebrity/277-find-the-celebrity.py
@@ -4,20 +4,18 @@
 
 class Solution:
     def findCelebrity(self, n: int) -> int:
-        def is_celebrity(i):
-            for j in range(n):
-                if i == j:
+        def is_celebrity(cand):
+            for o in range(n):
+                if cand == o:
                     continue
-                if knows(i, j) or not knows(j, i):
+                if knows(cand, o) or not knows(o, cand):
                     return False
             return True
-
-        candidate = 0
-        # step 1: narrow down candidate
+        
+        cand = 0
         for i in range(n):
-            if knows(candidate, i):
-                candidate = i
-        # step 2: verify it's REAL celebrity
-        if is_celebrity(candidate):
-            return candidate
+            if knows(cand, i):
+                cand = i
+        if is_celebrity(cand):
+            return cand
         return -1

f48e5cd90b99f14df3501eddf9351eb781e351f3
fxrc.cs <fxrcode@users.noreply.github.com>
2022-05-27 13:13:14 -0700

    Time: 43 ms (48.33%), Space: 13.8 MB (53.36%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -1,10 +1,10 @@
 class Solution:
     def numberOfSteps(self, num: int) -> int:
-        jump = 0
-        while num:
-            if num % 2 == 1:
-                num -= 1
+        steps = 0
+        binary = bin(num)[2:]
+        for bit in binary:
+            if bit == "1":
+                steps += 2
             else:
-                num //= 2
-            jump += 1
-        return jump
+                steps += 1
+        return steps - 1

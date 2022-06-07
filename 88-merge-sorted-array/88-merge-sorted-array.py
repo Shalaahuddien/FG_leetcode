448efc281e22182fcc8d608e09e6090796e4dc49
fxrc.cs <fxrcode@users.noreply.github.com>
2022-06-07 16:08:26 -0700

    Time: 47 ms (61.62%), Space: 13.9 MB (85.38%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -9,13 +9,12 @@ class Solution:
         # if n == 0:
         #     return
         i,j = m-1,n-1
-        k = m+n-1
-        while i >= 0 and j >= 0:
-            if nums1[i] >= nums2[j]:
-                nums1[k] = nums1[i]
-                i,k = i-1,k-1
+        for p in range(n+m-1, -1, -1):
+            if j < 0:
+                break
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[p] = nums1[i]
+                i -= 1
             else:
-                nums1[k] = nums2[j]
-                j,k = j-1,k-1
-        if j >= 0:
-            nums1[:k+1] = nums2[:j+1]
+                nums1[p] = nums2[j]
+                j -= 1

ee7896a59d89bc300b8919f2671ab7309a0b5550
fxrc.cs <fxrcode@users.noreply.github.com>
2022-06-28 13:40:20 -0700

    Time: 172 ms (76.42%), Space: 14.9 MB (17.40%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -1,15 +1,10 @@
 class Solution:
     def minDeletions(self, s: str) -> int:
-        s = "".join(sorted(s))
-        cnt = Counter(s)
-        freq = sorted(cnt.values(), reverse=True)
-        ans = 0
-        for i in range(1, len(freq)):
-            if freq[i] >= freq[i - 1]:
-                to = freq[i - 1] - 1
-                if to < 0:
-                    to = 0
-                ans += freq[i] - to
-                freq[i] = to
-        print("new: ", freq)
-        return ans
+        prev, keep = 2e9, 0
+        for freq in sorted(Counter(s).values(), reverse=True):
+            freq = min(prev - 1, freq)
+            if freq == 0:
+                break
+            keep += freq
+            prev = freq
+        return len(s) - keep

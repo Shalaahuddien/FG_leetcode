815d413c295474b19d8ab6913ec99bc5a19a8724
fxrc.cs <fxrcode@users.noreply.github.com>
2022-06-29 13:18:12 -0700

    Time: 144 ms (61.24%), Space: 14.4 MB (65.00%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
@@ -2,7 +2,6 @@ class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
         people.sort(key=lambda x: (-x[0], x[1]))
         res = []
-        for h,k in people:
-            res.insert(k, [h,k])
+        for p in people:
+            res.insert(p[1], p)
         return res
-        

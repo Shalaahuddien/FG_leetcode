ac6ee0ff82cd4aac8840cd97c08d1c049f9e17d3
fxrc.cs <fxrcode@users.noreply.github.com>
2022-07-01 15:38:07 -0700

    Time: 266 ms (37.81%), Space: 14.4 MB (83.23%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -2,7 +2,7 @@ class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
         boxTypes.sort(key=lambda x: -x[1])
         ans, cap = 0, truckSize
-        for b,u in boxTypes:
+        for b, u in boxTypes:
             if cap >= b:
                 ans += b * u
                 cap -= b

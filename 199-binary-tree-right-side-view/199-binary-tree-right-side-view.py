77f17115734a71cfbe5c33b64f32d23a8e809288
fxrc.cs <fxrcode@users.noreply.github.com>
2022-07-11 10:32:36 -0700

    Time: 67 ms (13.37%), Space: 14 MB (22.93%) - LeetHub

---
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        if not root:
+            return res
+        q = deque()
+        q.append(root)
+        while q:
+            qlen = len(q)
+            for i in range(qlen):
+                cur = q.popleft()
+                # handle result
+                if i == qlen - 1:
+                    res.append(cur.val)
+                if cur.left:
+                    q.append(cur.left)
+                if cur.right:
+                    q.append(cur.right)
+
+        return res

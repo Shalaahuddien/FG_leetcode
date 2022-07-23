5997d4743ae6d62c6aa7ebb0d05aa3bf652d8256
fxrc.cs <fxrcode@users.noreply.github.com>
2022-07-23 15:59:50 -0700

    Time: 5738 ms (20.90%), Space: 34.1 MB (56.61%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -1,8 +1,18 @@
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        arr, res = [], []
+        res, arr = [], []
+        # insert the given numbers from right to left, so only numbers after
+        # the current one are considered for each number in the array
         for n in nums[::-1]:
-            i = bisect.bisect_left(arr, n)
-            res.append(i)
-            arr.insert(i, n)
+            # use binary search to find the insertion point of the current
+            # number in the new array, which indicates the count of numbers
+            # smaller than the current one so far
+            idx = bisect_left(arr, n)
+            # the count is what we need for the result
+            res.append(idx)
+            # insert the current number into the new array
+            arr.insert(idx, n)
+        # reverse the results since the given numbers are traversed in a
+        # reversed order
+        print("arr", arr)
         return res[::-1]

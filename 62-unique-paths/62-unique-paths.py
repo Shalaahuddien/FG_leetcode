2a98fb2f4004466329ac6d2293a131dda9245df0
fxrc.cs <fxrcode@users.noreply.github.com>
2022-08-01 15:43:59 -0700

    Time: 37 ms (84.11%), Space: 14.1 MB (10.96%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,8 +1,17 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        DIR = [(0, 1), (1, 0)]
+
         @cache
-        def dfs(i, j):
-            if i >= m or j >= n:      return 0
-            if i == m-1 and j == n-1: return 1
-            return dfs(i+1, j) + dfs(i, j+1)
-        return dfs(0, 0)
+        def dp(i, j):
+            if (i, j) == (0, 0):
+                return 1
+
+            ans = 0
+            for dx, dy in DIR:
+                ii, jj = i - dx, j - dy
+                if 0 <= ii < m and 0 <= jj < n:
+                    ans += dp(ii, jj)
+            return ans
+
+        return dp(m - 1, n - 1)

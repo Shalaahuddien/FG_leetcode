9fe99fe0842828dfafc60652b141f0242e701512
fxrc.cs <fxrcode@users.noreply.github.com>
2022-09-25 16:56:57 -0700

    Time: 112 ms (6.85%), Space: 15.5 MB (88.93%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.py
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -6,22 +6,13 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
-        def dfs(node, target, path, res):
-            # print(node, target, path)
-            # if not node:
-            #     if target == 0:
-            #         return [path]
-            #     return [[]]
-            if not node:
-                return
-            if not node.left and not node.right:
-                if node.val == target:
-                    res.append(path + [node.val])
-                    return
-            target -= node.val
-            dfs(node.left, target, path + [node.val], res)
-            dfs(node.right, target, path + [node.val], res)
-
-        res = []
-        dfs(root, targetSum, [], res)
-        return res
+        def dfs(node, sm):
+            if not node: return []
+            if not node.left and not node.right and sm == node.val:
+                return [[node.val]]
+           
+            lft = dfs(node.left, sm - node.val)
+            rgh = dfs(node.right, sm - node.val)
+            return [cand + [node.val] for cand in lft + rgh]
+            
+        return [s[::-1] for s in dfs(root, targetSum)]

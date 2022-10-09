cc147a9b541ed0dea93ef447edabe57eeea7a39d
fxrc.cs <fxrcode@users.noreply.github.com>
2022-10-09 00:43:39 -0700

    Time: 234 ms (12.29%), Space: 20.2 MB (13.47%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
@@ -6,20 +6,16 @@
 #         self.right = right
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        def inorder(r):
-            if r:
-                inorder(r.left)
-                ino.append(r.val)
-                inorder(r.right)
-
-        ino = []
-        inorder(root)
-        i, j = 0, len(ino) - 1
-        while i < j:
-            if ino[i] + ino[j] == k:
-                return True
-            if ino[i] + ino[j] < k:
-                i += 1
-            else:
-                j -= 1
-        return False
+        def get(node, dr):
+            if not node: return
+            first, last = (node.left, node.right)[::dr]
+            yield from get(first, dr)
+            yield node.val
+            yield from get(last, dr)
+        
+        head, tail = get(root, 1), get(root, -1)
+        n1, n2 = next(head), next(tail)
+        while n1 < n2:
+            if n1 + n2 < k: n1 = next(head)
+            elif n1 + n2 > k: n2 = next(tail)
+            else: return True

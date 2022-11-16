fbbc7fd2e29813202a083e2058036f20c942b5e8
fxrc.cs <fxrcode@users.noreply.github.com>
2022-11-16 10:31:55 -0800

    Time: 46 ms (73.32%), Space: 14 MB (16.19%) - LeetHub

---
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.py
@@ -0,0 +1,8 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        return bisect_left(range(n), 0, key=lambda num: -guess(num))

3814ec3f384a49e3d0dff6ed40cf3329ceba0a91
fxrc.cs <fxrcode@users.noreply.github.com>
2022-12-02 11:25:50 -0800

    Time: 441 ms (38.40%), Space: 15.4 MB (16.80%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -1,5 +1,3 @@
 class Solution:
-    def closeStrings(self, word1: str, word2: str) -> bool:
-        C1, C2 = Counter(word1), Counter(word2)
-        return C1.keys() == C2.keys() \
-            and Counter(C1.values()) == Counter(C2.values())
+    def closeStrings(self, w1: str, w2: str) -> bool:
+        return set(w1) == set(w2) and Counter(Counter(w1).values()) == Counter(Counter(w2).values())

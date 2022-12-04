c2ae99caa6b5e91878bb35fcc4b0f5cb4d4cf016
fxrc.cs <fxrcode@users.noreply.github.com>
2022-12-03 20:25:00 -0800

    Time: 140 ms (20.24%), Space: 19 MB (17.20%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -0,0 +1,20 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        if not s: return s
+    
+        # Determine the frequency of each character.
+        counts = collections.Counter(s)
+        max_freq = max(counts.values())
+
+        # Bucket sort the characters by frequency.
+        buckets = [[] for _ in range(max_freq + 1)]
+        for c, i in counts.items():
+            buckets[i].append(c)
+
+        # Build up the string.
+        string_builder = []
+        for i in range(len(buckets) - 1, 0, -1):
+            for c in buckets[i]:
+                string_builder.append(c * i)
+
+        return "".join(string_builder)

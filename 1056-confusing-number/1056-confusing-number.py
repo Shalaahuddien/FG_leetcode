f527b9207c8867b8b74af5586d9d2a62fa761a37
fxrc.cs <fxrcode@users.noreply.github.com>
2023-01-01 14:40:24 -0800

    Time: 28 ms (96.43%), Space: 13.8 MB (62.76%) - LeetHub

--- 1056-confusing-number/1056-confusing-number.py
+++ 1056-confusing-number/1056-confusing-number.py
@@ -1,11 +1,8 @@
 class Solution:
-    def confusingNumber(self, n: int) -> bool:
-        mp = {0:0, 1:1, 6:9,8:8,9:6}
-        y = n
-        x = 0
-        while n:
-            n,m = divmod(n,10)
-            if m not in mp:
-                return False
-            x = x*10 + mp[m]
+    def confusingNumber(self, N: int) -> bool:
+        x, y, cmap = N, 0, {0:0,1:1,6:9,8:8,9:6}
+        while N:
+            n, m = divmod(N, 10)
+            if m not in cmap: return False
+            N, y = n, y*10 + cmap[m]
         return x != y

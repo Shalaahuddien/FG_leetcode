504372c0820da586839d60dae4bc0ef8cc6835c4
fxrc.cs <fxrcode@users.noreply.github.com>
2023-01-16 08:45:42 -0800

    Time: 248 ms (98.92%), Space: 52.9 MB (84.95%) - LeetHub

--- 1533-find-the-index-of-the-large-integer/1533-find-the-index-of-the-large-integer.py
+++ 1533-find-the-index-of-the-large-integer/1533-find-the-index-of-the-large-integer.py
@@ -17,16 +17,18 @@
 class Solution:
     def getIndex(self, reader: 'ArrayReader') -> int:
         n = reader.length()
-        lo, hi = 0, n - 1
-        while lo < hi:
-            mid = lo + (hi - lo) // 2
-            if (hi - lo + 1) % 2 == 0:
-                compare = reader.compareSub(lo, mid, mid + 1, hi)
-            else:
-                compare = reader.compareSub(lo, mid, mid, hi)
-            if compare >= 0:
-                hi = mid
-            else:
-                lo = mid + 1
+        l, r = 0, n - 1
 
-        return lo
+        while l < r:
+            mid = (l + r) // 2
+            if (r - l + 1) % 2 == 0:
+                cmp = reader.compareSub(l, mid, mid + 1, r)
+            else:
+                cmp = reader.compareSub(l, mid, mid, r)
+
+            if cmp >= 0:
+                r = mid
+            else:
+                l = mid + 1
+
+        return l

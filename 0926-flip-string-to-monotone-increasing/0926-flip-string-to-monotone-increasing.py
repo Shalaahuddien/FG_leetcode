73d278ea492012512077a0b16e4c69c08258841b
fxrc.cs <fxrcode@users.noreply.github.com>
2023-01-17 20:07:10 -0800

    Time: 147 ms (89.54%), Space: 15.1 MB (53.59%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
@@ -1,10 +1,9 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
-        s = [int(i) for i in s] + [1]
-        
-        @cache
-        def dp(i, k):
-            if i == -1: return 0
-            return min([dp(i-1, j) + int(k != s[i]) for j in range(k + 1)])
-           
-        return dp(len(s) - 1, 1)
+        ans, num = 0, 0
+        for c in s:
+            if c == "0":
+                ans = min(num, ans + 1)
+            else:
+                num += 1
+        return ans

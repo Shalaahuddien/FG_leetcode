a2b6ba72c64889193e7bba23e91af483998f2dbc
fxrc.cs <fxrcode@users.noreply.github.com>
2023-01-24 08:44:42 -0800

    Time: 152 ms (51.86%), Space: 13.9 MB (93.18%) - LeetHub

---
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -0,0 +1,17 @@
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        n = len(board)
+        need = {1: 0}
+        bfs = [1]
+        for x in bfs:
+            for i in range(x + 1, x + 7):
+                a, b = divmod(i - 1, n)
+                nxt = board[~a][b if a % 2 == 0 else ~b]
+                if nxt > 0:
+                    i = nxt
+                if i == n * n:
+                    return need[x] + 1
+                if i not in need:
+                    need[i] = need[x] + 1
+                    bfs.append(i)
+        return -1

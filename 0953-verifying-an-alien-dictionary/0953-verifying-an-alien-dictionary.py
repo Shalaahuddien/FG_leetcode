a043f93a6e3e5ecba125330a2ff1da12faa20c8d
fxrc.cs <fxrcode@users.noreply.github.com>
2023-02-01 21:00:42 -0800

    Time: 37 ms (77.62%), Space: 13.9 MB (31.27%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        ord_d = {l:i for i, l in enumerate(order)}
+         
+        for w1, w2 in zip(words, words[1:]):
+            for i, j in zip(w1, w2):
+                if i != j:
+                    if ord_d[i] > ord_d[j]: return False
+                    break
+            if w1.startswith(w2) and w1 != w2: return False
+            
+        return True
